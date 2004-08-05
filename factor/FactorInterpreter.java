5207ee870013cd532410ab0f2d089be22eb05ccc
Slava Pestov <slava@factorcode.org>
2004-08-05 06:36:09 +0000

    devel guide work

--- factor/FactorInterpreter.java
+++ factor/FactorInterpreter.java
@@ -35,7 +35,7 @@ import java.io.*;
 
 public class FactorInterpreter implements FactorObject, Runnable
 {
-	public static final String VERSION = "0.60.9";
+	public static final String VERSION = "0.60.10";
 
 	// command line arguments are stored here.
 	public Cons args;
