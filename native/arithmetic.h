e074e366822053cd9ae90381f5dc8efd98d1b7a6
Slava Pestov <slava@factorcode.org>
2004-12-13 21:46:30 +0000

    remove unneeded DLLEXPORT and obsolete optimizer tests

--- native/arithmetic.h
+++ native/arithmetic.h
@@ -1,6 +1,6 @@
 #include "factor.h"
 
-DLLEXPORT CELL arithmetic_type(CELL obj1, CELL obj2);
+CELL arithmetic_type(CELL obj1, CELL obj2);
 void primitive_arithmetic_type(void);
 
 bool realp(CELL tagged);
