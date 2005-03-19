4bee6179a4196994d2665af6faa4471733f1c99c
Mackenzie Straight <eizneckam@gmail.com>
2005-03-19 21:55:28 +0000

    integer types

--- native/factor.h
+++ native/factor.h
@@ -62,6 +62,15 @@ CELL cs_bot;
 #include <string.h>
 #include <time.h>
 
+typedef unsigned char uint8_t; 	 
+typedef unsigned short uint16_t; 	 
+typedef unsigned int uint32_t; 	 
+typedef unsigned long long uint64_t; 	 
+typedef signed char int8_t; 	 
+typedef signed short int16_t; 	 
+typedef signed int int32_t; 	 
+typedef signed long long int64_t;
+
 #ifdef WIN32
 	#include <windows.h>
 
