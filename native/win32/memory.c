ab97dccc66891aab97eee2ac99f344a767e09525
erg <erg@trifocus.net>
2006-03-25 08:56:01 +0000

    win32 runtime fix for dealloc_bounded_block

--- native/win32/memory.c
+++ native/win32/memory.c
@@ -2,35 +2,34 @@
 
 BOUNDED_BLOCK *alloc_bounded_block(CELL size)
 {
-	SYSTEM_INFO si;
-	char *mem;
-	DWORD ignore;
-	
-	GetSystemInfo(&si);
-	mem = (char *)VirtualAlloc(NULL, si.dwPageSize*2 + size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-	if(!mem)
-		fatal_error("VirtualAlloc() failed in alloc_bounded_block()",0);
-	
-	if (!VirtualProtect(mem, si.dwPageSize, PAGE_NOACCESS, &ignore))
-		fatal_error("Cannot allocate low guard page", (CELL)mem);
-	
-	if (!VirtualProtect(mem+size+si.dwPageSize, si.dwPageSize, PAGE_NOACCESS, &ignore))
-		fatal_error("Cannot allocate high guard page", (CELL)mem);
-	
-	BOUNDED_BLOCK *retval = safe_malloc(sizeof(BOUNDED_BLOCK));
-	
-	retval->start = mem + si.dwPageSize;
-	retval->size = size;
-	
-	return retval;
+    SYSTEM_INFO si;
+    char *mem;
+    DWORD ignore;
+
+    GetSystemInfo(&si);
+    if((mem = (char *)VirtualAlloc(NULL, si.dwPageSize*2 + size, MEM_COMMIT, PAGE_EXECUTE_READWRITE)) == 0)
+        fatal_error("VirtualAlloc() failed in alloc_bounded_block()",0);
+
+    if (!VirtualProtect(mem, si.dwPageSize, PAGE_NOACCESS, &ignore))
+        fatal_error("Cannot allocate low guard page", (CELL)mem);
+
+    if (!VirtualProtect(mem+size+si.dwPageSize, si.dwPageSize, PAGE_NOACCESS, &ignore))
+        fatal_error("Cannot allocate high guard page", (CELL)mem);
+
+    BOUNDED_BLOCK *block = safe_malloc(sizeof(BOUNDED_BLOCK));
+
+    block->start = (int)mem + si.dwPageSize;
+    block->size = size;
+
+    return block;
 }
 
 void dealloc_bounded_block(BOUNDED_BLOCK *block)
 {
-	
-	SYSTEM_INFO si;
-	GetSystemInfo(&si);
-	if(!VirtualFree(block->start - si.dwPageSize, si.dwPageSize*2 + block->size, MEM_DECOMMIT))
-		fatal_error("VirtualFree() failed",0);
-	free(block);
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    if(!VirtualFree((void*)(block->start - si.dwPageSize), 0, MEM_RELEASE))
+        fatal_error("VirtualFree() failed",0);
+    free(block);
 }
+
