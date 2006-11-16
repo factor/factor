e371ffcae6d2b29cd986160e8ce6fb7b34617efc
slava <slava@factorcode.org>
2006-11-16 07:12:03 +0000

    Working on amd64 stack traces

--- vm/cpu-amd64.h
+++ vm/cpu-amd64.h
@@ -5,3 +5,10 @@ register CELL rs asm("r15");
 register CELL cards_offset asm("r13");
 
 INLINE void flush_icache(CELL start, CELL len) {}
+
+void *native_stack_pointer(void);
+
+typedef struct _F_STACK_FRAME {
+	struct _F_STACK_FRAME *previous;
+	CELL *return_address;
+} F_STACK_FRAME;
