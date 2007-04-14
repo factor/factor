ea94662abd7abe4b19ccd6e0a7eaf17211792db2
Doug Coleman <doug.coleman@gmail.com>
2007-04-14 05:49:09 -0500

    NetBSD x86/64 fixes

--- vm/os-netbsd.h
+++ vm/os-netbsd.h
@@ -1,6 +1,5 @@
 #include <ucontext.h>
 
-#define ucontext_stack_pointer(uap) ((void *)_UC_MACHINE_SP((ucontext_t *)uap))
 #define UAP_PROGRAM_COUNTER(uap)    _UC_MACHINE_PC((ucontext_t *)uap)
 
 #define UNKNOWN_TYPE_P(file) ((file)->d_type == DT_UNKNOWN)
