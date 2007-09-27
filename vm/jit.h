9a0ac0e97558e58ffb717e28fb8ed26d9e82edde
U-C4\Administrator <Administrator@c4.(none)>
2007-09-27 15:10:37 -0500

    Attempt to merge in changes F_FASTCALL

--- vm/jit.h
+++ vm/jit.h
@@ -1,2 +1,2 @@
-DLLEXPORT FASTCALL CELL jit_compile(CELL tagged, F_STACK_FRAME *stack);
+DLLEXPORT F_FASTCALL CELL jit_compile(CELL tagged, F_STACK_FRAME *stack);
 XT quot_offset_to_pc(F_QUOTATION *quot, F_FIXNUM offset);
