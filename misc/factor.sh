da29219d7b3fe0e87cb54e6ae4c8ae0810471fe0
Doug Coleman <doug.coleman@gmail.com>
2008-03-08 11:44:28 -0600

    better fix for misc/factor.sh

--- misc/factor.sh
+++ misc/factor.sh
@@ -95,7 +95,6 @@ check_installed_programs() {
     ensure_program_installed md5sum md5
     ensure_program_installed cut
     case $OS in
-        macosx) ensure_program_installed git port;;
         netbsd) ensure_program_installed gmake;;
     esac
     check_gcc_version
