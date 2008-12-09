9ad51490afe7d5354d8575f8bf943596d44bc2dc
Slava Pestov <slava@slava-pestovs-macbook-pro.local>
2008-12-09 16:26:30 -0600

    Fix GC crash with -generations=2

--- vm/data_gc.c
+++ vm/data_gc.c
@@ -32,9 +32,7 @@ F_DATA_HEAP *alloc_data_heap(CELL gens,
 	data_heap->gen_count = gens;
 
 	CELL total_size;
-	if(data_heap->gen_count == 1)
-		total_size = 2 * tenured_size;
-	else if(data_heap->gen_count == 2)
+	if(data_heap->gen_count == 2)
 		total_size = young_size + 2 * tenured_size;
 	else if(data_heap->gen_count == 3)
 		total_size = young_size + 2 * aging_size + 2 * tenured_size;
