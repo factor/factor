9a68a262fd759050cfb3aa070706b62c8a634c4d
Slava Pestov <slava@slava-pestovs-macbook-pro.local>
2009-04-30 19:07:13 -0500

    Clean up code duplication in quotations.c

--- vm/jit.h
+++ vm/jit.h
@@ -4,10 +4,17 @@ typedef struct {
 	F_GROWABLE_BYTE_ARRAY code;
 	F_GROWABLE_BYTE_ARRAY relocation;
 	F_GROWABLE_ARRAY literals;
+	bool computing_offset_p;
+	F_FIXNUM position;
+	CELL offset;
 } F_JIT;
 
 void jit_init(F_JIT *jit, CELL jit_type, CELL owner);
+
+void jit_compute_position(F_JIT *jit, CELL offset);
+
 F_CODE_BLOCK *jit_make_code_block(F_JIT *jit);
+
 void jit_dispose(F_JIT *jit);
 
 INLINE F_BYTE_ARRAY *code_to_emit(CELL template)
@@ -59,4 +66,22 @@ INLINE void jit_emit_subprimitive(F_JIT *jit, F_WORD *word)
 	UNREGISTER_UNTAGGED(word);
 
 	jit_emit(jit,word->subprimitive);
+}
+
+INLINE F_FIXNUM jit_get_position(F_JIT *jit)
+{
+	if(jit->computing_offset_p)
+	{
+		/* If this is still on, jit_emit() didn't clear it,
+		   so the offset was out of bounds */
+		return -1;
+	}
+	else
+		return jit->position;
+}
+
+INLINE void jit_set_position(F_JIT *jit, F_FIXNUM position)
+{
+	if(jit->computing_offset_p)
+		jit->position = position;
 }
--- vm/quotations.h
+++ vm/quotations.h
@@ -8,7 +8,7 @@ INLINE CELL tag_quotation(F_QUOTATION *quotation)
 void set_quot_xt(F_QUOTATION *quot, F_CODE_BLOCK *code);
 void jit_compile(CELL quot, bool relocate);
 F_FASTCALL CELL lazy_jit_compile_impl(CELL quot, F_STACK_FRAME *stack);
-F_FIXNUM quot_code_offset_to_scan(CELL quot, F_FIXNUM offset);
+F_FIXNUM quot_code_offset_to_scan(CELL quot, CELL offset);
 void primitive_array_to_quotation(void);
 void primitive_quotation_xt(void);
 void primitive_jit_compile(void);
