c830bd7ee2322ed55de29654191f3747ab226f4c
Samuel Tardieu <sam@rfc1149.net>
2009-05-04 14:45:15 +0200

    Make the primitives array const

--- vm/primitives.hpp
+++ vm/primitives.hpp
@@ -2,7 +2,7 @@ namespace factor
 {
 
 extern "C" typedef void (*primitive_type)();
-extern primitive_type primitives[];
+extern const primitive_type primitives[];
 
 #define PRIMITIVE(name) extern "C" void primitive_##name()
 
