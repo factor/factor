087483eea2b771c9e1e0527b33d04c36fe5c9e0a
Slava Pestov <slava@factorcode.org>
2009-05-04 06:43:20 -0500

    NetBSD build fix

--- vm/os-netbsd.cpp
+++ vm/os-netbsd.cpp
@@ -3,13 +3,13 @@
 namespace factor
 {
 
-extern int main();
+extern "C" int main();
 
 const char *vm_executable_path(void)
 {
 	static Dl_info info = {0};
 	if (!info.dli_fname)
-		dladdr(main, &info);
+		dladdr((void *)main, &info);
 	return info.dli_fname;
 }
 
