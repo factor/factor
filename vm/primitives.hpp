c627ed7a2294953b7b9d3b6cce382f5f7e8a2c41
Phil Dawes <phil@phildawes.net>
2009-08-17 21:37:04 +0100

    added stub PRIMITIVE_GETVM macro

--- vm/primitives.hpp
+++ vm/primitives.hpp
@@ -5,5 +5,5 @@ extern "C" typedef void (*primitive_type)();
 extern const primitive_type primitives[];
 
 #define PRIMITIVE(name) extern "C" void primitive_##name()
-
+#define PRIMITIVE_GETVM() vm->
 }
