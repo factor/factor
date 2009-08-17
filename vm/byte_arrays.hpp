f2cb6e6f6f7db2996dd432446531d4a314dc82cc
Phil Dawes <phil@phildawes.net>
2009-08-17 21:37:12 +0100

    moved byte_arrays.hpp inline functions to vm.hpp

--- vm/byte_arrays.hpp
+++ vm/byte_arrays.hpp
@@ -7,16 +7,5 @@ PRIMITIVE(byte_array);
 PRIMITIVE(uninitialized_byte_array);
 PRIMITIVE(resize_byte_array);
 
-struct growable_byte_array {
-	cell count;
-	gc_root<byte_array> elements;
-
-	growable_byte_array(factorvm *vm,cell capacity = 40) : count(0), elements(allot_byte_array(capacity),vm) { }
-
-	void append_bytes(void *elts, cell len);
-	void append_byte_array(cell elts);
-
-	void trim();
-};
 
 }
