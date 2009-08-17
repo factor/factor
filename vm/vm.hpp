88084a66ac79641a88509e80fccd277ca71b3e57
Phil Dawes <phil@phildawes.net>
2009-08-17 21:37:04 +0100

    moved contexts functions into vm

--- vm/vm.hpp
+++ vm/vm.hpp
@@ -2,6 +2,26 @@ namespace factor
 {
 
 struct factorvm {
+
+	// contexts
+	void reset_datastack();
+	void reset_retainstack();
+	void fix_stacks();
+	void save_stacks();
+	context *alloc_context();
+	void dealloc_context(context *old_context);
+	void nest_stacks();
+	void unnest_stacks();
+	void init_stacks(cell ds_size_, cell rs_size_);
+	bool stack_to_array(cell bottom, cell top);
+	cell array_to_stack(array *array, cell bottom);
+	inline void vmprim_datastack();
+	inline void vmprim_retainstack();
+	inline void vmprim_set_datastack();
+	inline void vmprim_set_retainstack();
+	inline void vmprim_check_datastack();
+	// next method here:
+
 };
 
 extern factorvm *vm;
