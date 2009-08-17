a249b484c47773193f731f0b24bc47b58d701357
Phil Dawes <phil@phildawes.net>
2009-08-17 21:37:12 +0100

    moved byte_arrays.hpp inline functions to vm.hpp

--- vm/vm.hpp
+++ vm/vm.hpp
@@ -929,6 +929,19 @@ struct growable_array {
 	void trim();
 };
 
+//byte_arrays.hpp
+struct growable_byte_array {
+	cell count;
+	gc_root<byte_array> elements;
+
+	growable_byte_array(factorvm *vm,cell capacity = 40) : count(0), elements(allot_byte_array(capacity),vm) { }
+
+	void append_bytes(void *elts, cell len);
+	void append_byte_array(cell elts);
+
+	void trim();
+};
+
 // next method here:
 
 
