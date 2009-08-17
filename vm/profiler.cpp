12d4f2f81259c209f5b3eacce7f39c75d8e947a9
Phil Dawes <phil@phildawes.net>
2009-08-17 21:37:16 +0100

    removed global functions from profiler

--- vm/profiler.cpp
+++ vm/profiler.cpp
@@ -9,10 +9,6 @@ void factorvm::init_profiler()
 	profiling_p = false;
 }
 
-void init_profiler()
-{
-	return vm->init_profiler();
-}
 
 /* Allocates memory */
 code_block *factorvm::compile_profiling_stub(cell word_)
@@ -25,10 +21,6 @@ code_block *factorvm::compile_profiling_stub(cell word_)
 	return jit.to_code_block();
 }
 
-code_block *compile_profiling_stub(cell word_)
-{
-	return vm->compile_profiling_stub(word_);
-}
 
 /* Allocates memory */
 void factorvm::set_profiling(bool profiling)
@@ -58,10 +50,6 @@ void factorvm::set_profiling(bool profiling)
 	iterate_code_heap(factor::relocate_code_block);
 }
 
-void set_profiling(bool profiling)
-{
-	return vm->set_profiling(profiling);
-}
 
 inline void factorvm::vmprim_profiling()
 {
--- vm/profiler.hpp
+++ vm/profiler.hpp
@@ -1,8 +1,6 @@
 namespace factor
 {
 
-void init_profiler();
-code_block *compile_profiling_stub(cell word);
 PRIMITIVE(profiling);
 
 }
