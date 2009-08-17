3be50057fa05160e06a5c8e72f36d7978093d613
Phil Dawes <phil@phildawes.net>
2009-08-17 21:37:11 +0100

    moved arrays.hpp inline functions to vm.hpp

--- vm/arrays.hpp
+++ vm/arrays.hpp
@@ -10,21 +10,9 @@ inline cell array_nth(array *array, cell slot)
 	return array->data()[slot];
 }
 
-inline void factorvm::set_array_nth(array *array, cell slot, cell value)
-{
-#ifdef FACTOR_DEBUG
-	assert(slot < array_capacity(array));
-	assert(array->h.hi_tag() == ARRAY_TYPE);
-	check_tagged_pointer(value);
-#endif
-	array->data()[slot] = value;
-	write_barrier(array);
-}
 
-inline void set_array_nth(array *array, cell slot, cell value)
-{
-	return vm->set_array_nth(array,slot,value);
-}
+
+
 
 array *allot_array(cell capacity, cell fill);
 
@@ -35,14 +23,5 @@ cell allot_array_4(cell v1, cell v2, cell v3, cell v4);
 PRIMITIVE(array);
 PRIMITIVE(resize_array);
 
-struct growable_array {
-	cell count;
-	gc_root<array> elements;
-
-	growable_array(factorvm *myvm, cell capacity = 10) : count(0), elements(allot_array(capacity,F),myvm) {}
-
-	void add(cell elt);
-	void trim();
-};
 
 }
