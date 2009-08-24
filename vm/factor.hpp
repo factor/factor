91e91e68eb73afee05a82a4ef816dac8866339eb
Phil Dawes <phil@phildawes.net>
2009-08-24 20:59:05 +0100

    Start windows factor in a spawned thread (for testing)

--- vm/factor.hpp
+++ vm/factor.hpp
@@ -2,6 +2,6 @@ namespace factor
 {
 
 VM_C_API void start_standalone_factor(int argc, vm_char **argv);
-VM_C_API void start_standalone_factor_in_new_thread(int argc, vm_char **argv);
+VM_C_API void *start_standalone_factor_in_new_thread(int argc, vm_char **argv);
 
 }
