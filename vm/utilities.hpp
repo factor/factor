f60b1e1dd0105dfcff31f497eae43f4a3f82f8c3
Phil Dawes <phil@phildawes.net>
2009-08-25 08:20:58 +0100

    moved utility functions and fatal_error out of vm struct since doesn't need state

--- vm/utilities.hpp
+++ vm/utilities.hpp
@@ -1,4 +1,12 @@
 namespace factor
 {
-
+	void *safe_malloc(size_t size);
+	vm_char *safe_strdup(const vm_char *str);
+	void print_string(const char *str);
+	void nl();
+	void print_cell(cell x);
+	void print_cell_hex(cell x);
+	void print_cell_hex_pad(cell x);
+	void print_fixnum(fixnum x);
+	cell read_cell_hex();
 }
