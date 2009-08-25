4a7516ff176257ba0b51ddfa7546300557253f38
Phil Dawes <phil@phildawes.net>
2009-08-25 18:17:14 +0100

    cleaned up code a bit, added multithreaded mode flags

--- vm/main-unix.cpp
+++ vm/main-unix.cpp
@@ -2,6 +2,11 @@
 
 int main(int argc, char **argv)
 {
-	factor::start_standalone_factor(argc,argv);
+	#ifdef FACTOR_MULTITHREADED
+	  factor::THREADHANDLE thread = factor::start_standalone_factor_in_new_thread(argc,argv);
+	  pthread_join(thread,NULL);
+    #else
+	  factor::start_standalone_factor(argc,argv);
+	#endif
 	return 0;
 }
--- vm/primitives.hpp
+++ vm/primitives.hpp
@@ -4,15 +4,10 @@ namespace factor
 #if defined(FACTOR_X86)
   extern "C" __attribute__ ((regparm (1))) typedef void (*primitive_type)(void *myvm);
   #define PRIMITIVE(name) extern "C" __attribute__ ((regparm (1)))  void primitive_##name(void *myvm)
-  #define PRIMITIVE_GETVM() ((factorvm*)myvm)
 #else
   extern "C" typedef void (*primitive_type)(void *myvm);
   #define PRIMITIVE(name) extern "C" void primitive_##name(void *myvm)
-  #define PRIMITIVE_GETVM() vm
 #endif
 
 extern const primitive_type primitives[];
-#define PRIMITIVE_OVERFLOW_GETVM() vm
-#define VM_PTR vm
-#define ASSERTVM() 
 }
