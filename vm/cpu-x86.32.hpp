1f011f118bbcab4036560c3d32937b8e9447b9d1
Phil Dawes <phil@phildawes.net>
2009-09-02 08:06:00 +0100

    converted box_* integer functions to use vm (x86 windows)

--- vm/cpu-x86.32.hpp
+++ vm/cpu-x86.32.hpp
@@ -7,8 +7,4 @@ register cell ds asm("esi");
 register cell rs asm("edi");
 
 #define VM_ASM_API VM_C_API __attribute__ ((regparm (2)))
-#undef VM_PTR
-#define VM_PTR myvm
-#undef ASSERTVM
-#define ASSERTVM() assert(vm==myvm)
 }
