abb3c713405a9d942559c6661645307f732a7d7e
Joe Groff <arcata@gmail.com>
2009-09-14 00:00:01 -0500

    fix compilation on freebsd

--- vm/os-freebsd-x86.32.hpp
+++ vm/os-freebsd-x86.32.hpp
@@ -13,12 +13,12 @@ inline static void *ucontext_stack_pointer(void *uap)
 inline static unsigned int uap_fpu_status(void *uap)
 {
         ucontext_t *ucontext = (ucontext_t *)uap;
-        if (uap->uc_mcontext.mc_fpformat == _MC_FPFMT_387) {
+        if (ucontext->uc_mcontext.mc_fpformat == _MC_FPFMT_387) {
             struct save87 *x87 = (struct save87 *)(&ucontext->uc_mcontext.mc_fpstate);
-            return x87->en_sw;
-        } else if (uap->uc_mcontext.mc_fpformat == _MC_FPFMT_XMM) {
+            return x87->sv_env.en_sw;
+        } else if (ucontext->uc_mcontext.mc_fpformat == _MC_FPFMT_XMM) {
             struct savexmm *xmm = (struct savexmm *)(&ucontext->uc_mcontext.mc_fpstate);
-            return xmm->en_sw | xmm->en_mxcsr;
+            return xmm->sv_env.en_sw | xmm->sv_env.en_mxcsr;
         } else
             return 0;
 }
@@ -28,11 +28,11 @@ inline static void uap_clear_fpu_status(void *uap)
         ucontext_t *ucontext = (ucontext_t *)uap;
         if (uap->uc_mcontext.mc_fpformat == _MC_FPFMT_387) {
             struct save87 *x87 = (struct save87 *)(&ucontext->uc_mcontext.mc_fpstate);
-            x87->en_sw = 0;
+            x87->sv_env.en_sw = 0;
         } else if (uap->uc_mcontext.mc_fpformat == _MC_FPFMT_XMM) {
             struct savexmm *xmm = (struct savexmm *)(&ucontext->uc_mcontext.mc_fpstate);
-            xmm->en_sw = 0;
-            xmm->en_mxcsr &= 0xffffffc0;
+            xmm->sv_env.en_sw = 0;
+            xmm->sv_env.en_mxcsr &= 0xffffffc0;
         }
 }
 
