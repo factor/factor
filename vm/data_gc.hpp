b4e36608da2f842c9910b018d5a86033dbe9e93d
Slava Pestov <slava@shill.local>
2009-10-05 05:27:49 -0500

    compiler.cfg: remove _gc instruction, it doesn't need to exist, and change GC checks to ensure that the right amount of space is available instead of blindly checking for 1Kb

--- vm/data_gc.hpp
+++ vm/data_gc.hpp
@@ -58,11 +58,6 @@ struct gc_state {
 	}
 };
 
-/* We leave this many bytes free at the top of the nursery so that inline
-allocation (which does not call GC because of possible roots in volatile
-registers) does not run out of memory */
-static const cell allot_buffer_zone = 1024;
-
 VM_C_API void inline_gc(cell *gc_roots_base, cell gc_roots_size, factor_vm *myvm);
 
 }
