9314e8cdf22c0980a9cb9433a029daf51212e65a
Slava Pestov <slava@slava-pestovs-macbook-pro.local>
2009-10-15 22:06:43 -0500

    vm: remove some dead code

--- vm/tenured_space.hpp
+++ vm/tenured_space.hpp
@@ -3,10 +3,6 @@ namespace factor
 
 struct tenured_space : old_space {
 	tenured_space(cell size, cell start) : old_space(size,start) {}
-
-	bool is_nursery_p() { return false; }
-	bool is_aging_p()   { return false; }
-	bool is_tenured_p() { return true; }
 };
 
 }
