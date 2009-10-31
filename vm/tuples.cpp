b202371d620a3439aeb7560bb76833634625fdf7
Slava Pestov <slava@slava-pestovs-macbook-pro.local>
2009-10-31 02:58:00 -0500

    vm: faster tuple allocation primitives

--- vm/tuples.cpp
+++ vm/tuples.cpp
@@ -3,33 +3,29 @@
 namespace factor
 {
 
-/* push a new tuple on the stack */
-tuple *factor_vm::allot_tuple(cell layout_)
-{
-	gc_root<tuple_layout> layout(layout_,this);
-	gc_root<tuple> t(allot<tuple>(tuple_size(layout.untagged())),this);
-	t->layout = layout.value();
-	return t.untagged();
-}
-
 /* push a new tuple on the stack, filling its slots with f */
 void factor_vm::primitive_tuple()
 {
 	gc_root<tuple_layout> layout(dpop(),this);
-	tuple *t = allot_tuple(layout.value());
-	cell size = (tuple_size(layout.untagged()) - 1) * sizeof(cell);
-	memset_cell(t->data(),false_object,size);
-	dpush(tag<tuple>(t));
+	tagged<tuple> t(allot<tuple>(tuple_size(layout.untagged())));
+	t->layout = layout.value();
+
+	memset_cell(t->data(),false_object,tuple_size(layout.untagged()) - sizeof(cell));
+
+	dpush(t.value());
 }
 
 /* push a new tuple on the stack, filling its slots from the stack */
 void factor_vm::primitive_tuple_boa()
 {
 	gc_root<tuple_layout> layout(dpop(),this);
-	gc_root<tuple> t(allot_tuple(layout.value()),this);
+	tagged<tuple> t(allot<tuple>(tuple_size(layout.untagged())));
+	t->layout = layout.value();
+
 	cell size = untag_fixnum(layout.untagged()->size) * sizeof(cell);
-	memcpy(t->data(),(cell *)(ds - (size - sizeof(cell))),size);
+	memcpy(t->data(),(cell *)(ds - size + sizeof(cell)),size);
 	ds -= size;
+
 	dpush(t.value());
 }
 
