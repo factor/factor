4061951d1c94bd9cd0db6500e95506c80122c297
Slava Pestov <slava@slava-pestovs-macbook-pro.local>
2009-11-05 21:49:03 -0600

    vm: simpler object space implementation. begin-scan/next-object/end-scan primitives replaced by a single all-instances primitive

--- vm/data_heap.cpp
+++ vm/data_heap.cpp
@@ -226,82 +226,42 @@ void factor_vm::primitive_data_room()
 	dpush(tag<byte_array>(byte_array_from_value(&room)));
 }
 
-/* Disables GC and activates next-object ( -- obj ) primitive */
-void factor_vm::begin_scan()
+struct object_accumulator {
+	cell type;
+	std::vector<cell> objects;
+
+	explicit object_accumulator(cell type_) : type(type_) {}
+
+	void operator()(object *obj)
+	{
+		if(type == TYPE_COUNT || obj->h.hi_tag() == type)
+			objects.push_back(tag_dynamic(obj));
+	}
+};
+
+cell factor_vm::instances(cell type)
 {
-	heap_scan_ptr = data->tenured->first_object();
+	object_accumulator accum(type);
+	each_object(accum);
+	cell object_count = accum.objects.size();
+
 	gc_off = true;
-}
-
-void factor_vm::end_scan()
-{
+	array *objects = allot_array(object_count,false_object);
+	memcpy(objects->data(),&accum.objects[0],object_count * sizeof(cell));
 	gc_off = false;
+
+	return tag<array>(objects);
 }
 
-void factor_vm::primitive_begin_scan()
+void factor_vm::primitive_all_instances()
 {
-	begin_scan();
+	primitive_full_gc();
+	dpush(instances(TYPE_COUNT));
 }
-
-cell factor_vm::next_object()
-{
-	if(!gc_off)
-		general_error(ERROR_HEAP_SCAN,false_object,false_object,NULL);
-
-	if(heap_scan_ptr)
-	{
-		cell current = heap_scan_ptr;
-		heap_scan_ptr = data->tenured->next_object_after(heap_scan_ptr);
-		return tag_dynamic((object *)current);
-	}
-	else
-		return false_object;
-}
-
-/* Push object at heap scan cursor and advance; pushes f when done */
-void factor_vm::primitive_next_object()
-{
-	dpush(next_object());
-}
-
-/* Re-enables GC */
-void factor_vm::primitive_end_scan()
-{
-	gc_off = false;
-}
-
-struct word_counter {
-	cell count;
-
-	explicit word_counter() : count(0) {}
-
-	void operator()(cell obj)
-	{
-		if(tagged<object>(obj).type_p(WORD_TYPE))
-			count++;
-	}
-};
-
-struct word_accumulator {
-	growable_array words;
-
-	explicit word_accumulator(int count,factor_vm *vm) : words(vm,count) {}
-
-	void operator()(cell obj)
-	{
-		if(tagged<object>(obj).type_p(WORD_TYPE))
-			words.add(obj);
-	}
-};
 
 cell factor_vm::find_all_words()
 {
-	word_counter counter;
-	each_object(counter);
-	word_accumulator accum(counter.count,this);
-	each_object(accum);
-	accum.words.trim();
-	return accum.words.elements.value();
+	return instances(WORD_TYPE);
 }
 
 }
