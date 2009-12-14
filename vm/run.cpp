0a3c5f5ac686d02b29a0b8bef1d89a76a01808e0
Slava Pestov <slava@factorcode.org>
2009-12-14 01:09:04 -0600

    vm: fep out if monotonic counter decreases

--- vm/run.cpp
+++ vm/run.cpp
@@ -15,7 +15,10 @@ void factor_vm::primitive_system_micros()
 
 void factor_vm::primitive_nano_count()
 {
-	box_unsigned_8(nano_count());
+	u64 nanos = nano_count();
+	if(nanos < last_nano_count) critical_error("Monotonic counter decreased",0);
+	last_nano_count = nanos;
+	box_unsigned_8(nanos);
 }
 
 void factor_vm::primitive_sleep()
