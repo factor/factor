2aa4d3d4325f591a152d695a104a592466b7bdb3
Slava Pestov <slava@slava-pestovs-macbook-pro.local>
2010-01-14 02:18:49 +1300

    vm: fix compilation on 64-bit platforms

--- vm/master.hpp
+++ vm/master.hpp
@@ -30,6 +30,24 @@
 #include <vector>
 #include <iostream>
 
+/* Detect target CPU type */
+#if defined(__arm__)
+	#define FACTOR_ARM
+#elif defined(__amd64__) || defined(__x86_64__)
+	#define FACTOR_AMD64
+	#define FACTOR_64
+#elif defined(i386) || defined(__i386) || defined(__i386__) || defined(WIN32)
+	#define FACTOR_X86
+#elif defined(__POWERPC__) || defined(__ppc__) || defined(_ARCH_PPC)
+	#define FACTOR_PPC
+#else
+	#error "Unsupported architecture"
+#endif
+
+#ifdef WIN32
+	#define WINDOWS
+#endif
+
 /* Forward-declare this since it comes up in function prototypes */
 namespace factor
 {
--- vm/platform.hpp
+++ vm/platform.hpp
@@ -1,20 +1,3 @@
-#if defined(__arm__)
-	#define FACTOR_ARM
-#elif defined(__amd64__) || defined(__x86_64__)
-	#define FACTOR_AMD64
-	#define FACTOR_64
-#elif defined(i386) || defined(__i386) || defined(__i386__) || defined(WIN32)
-	#define FACTOR_X86
-#elif defined(__POWERPC__) || defined(__ppc__) || defined(_ARCH_PPC)
-	#define FACTOR_PPC
-#else
-	#error "Unsupported architecture"
-#endif
-
-#ifdef WIN32
-	#define WINDOWS
-#endif
-
 #if defined(WINDOWS)
 	#if defined(WINCE)
 		#include "os-windows-ce.hpp"
