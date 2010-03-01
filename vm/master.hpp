2e5887d519c0bab41d5c009e5e0bcecd76686c0f
Joe Groff <arcata@gmail.com>
2010-03-01 13:32:07 -0800

    record the C++ compiler version in the VM, and expose it with a vm-compiler word

--- vm/master.hpp
+++ vm/master.hpp
@@ -29,6 +29,19 @@
 #include <vector>
 #include <iostream>
 
+/* Record compiler version */
+#if defined(__clang__)
+	#define FACTOR_COMPILER_VERSION "Clang (GCC " __VERSION__ ")"
+#elif defined(__INTEL_COMPILER)
+	#define FACTOR_COMPILER_VERSION "Intel C Compiler " #__INTEL_COMPILER
+#elif defined(__GNUC__)
+	#define FACTOR_COMPILER_VERSION "GCC " __VERSION__
+#elif defined(_MSC_FULL_VER)
+	#define FACTOR_COMPILER_VERSION "Microsoft Visual C++ " #_MSC_FULL_VER
+#else
+	#define FACTOR_COMPILER_VERSION "unknown"
+#endif
+
 /* Detect target CPU type */
 #if defined(__arm__)
 	#define FACTOR_ARM
