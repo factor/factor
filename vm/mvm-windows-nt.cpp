7e2c6feeb563bedfa6c9b05ea7b33c7ab6785253
Slava Pestov <slava@slava-pestovs-macbook-pro.local>
2010-03-29 02:27:45 -0400

    vm: fix typos

--- vm/mvm-windows-nt.cpp
+++ vm/mvm-windows-nt.cpp
@@ -7,13 +7,13 @@ DWORD current_vm_tls_key;
 
 void init_mvm()
 {
-	if ((current_vm_tls_key = TlsAlloc()) == TLS_OUT_OF_INDEXES)
+	if((current_vm_tls_key = TlsAlloc()) == TLS_OUT_OF_INDEXES)
 		fatal_error("TlsAlloc() failed",0);
 }
 
 void register_vm_with_thread(factor_vm *vm)
 {
-	if (!TlsSetValue(current_vm_tls_key, vm))
+	if(!TlsSetValue(current_vm_tls_key, vm))
 		fatal_error("TlsSetValue() failed",0);
 }
 
