df0964d49792ab323542eb28eedbeaddf2a4bd6d
Slava Pestov <slava@factorcode.org>
2010-07-31 18:33:53 -0400

    vm: fix compile error on Linux if inotify is not available (reported by Alec Barryman)

--- vm/os-linux.cpp
+++ vm/os-linux.cpp
@@ -45,19 +45,19 @@ VM_C_API int inotify_rm_watch(int fd, u32 wd)
 
 VM_C_API int inotify_init()
 {
-	parent->not_implemented_error();
+	current_vm()->not_implemented_error();
 	return -1;
 }
 
 VM_C_API int inotify_add_watch(int fd, const char *name, u32 mask)
 {
-	parent->not_implemented_error();
+	current_vm()->not_implemented_error();
 	return -1;
 }
 
 VM_C_API int inotify_rm_watch(int fd, u32 wd)
 {
-	parent->not_implemented_error();
+	current_vm()->not_implemented_error();
 	return -1;
 }
 
