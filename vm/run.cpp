6007032e86c3a6e3b69bdcf37ebeb4cd0a399637
Slava Pestov <slava@factorcode.org>
2010-08-02 20:50:34 -0400

    vm: hard tabs, indent width 8

--- vm/run.cpp
+++ vm/run.cpp
@@ -11,12 +11,13 @@ void factor_vm::primitive_exit()
 void factor_vm::primitive_nano_count()
 {
 	u64 nanos = nano_count();
-	if(nanos < last_nano_count) {
-            std::cout << "Monotonic counter decreased from 0x";
-            std::cout << std::hex << last_nano_count;
-            std::cout << " to 0x" << nanos << "." << std::dec << "\n";
-            std::cout << "Please report this error.\n";
-            current_vm()->factorbug();
+	if(nanos < last_nano_count)
+	{
+		std::cout << "Monotonic counter decreased from 0x";
+		std::cout << std::hex << last_nano_count;
+		std::cout << " to 0x" << nanos << "." << std::dec << "\n";
+		std::cout << "Please report this error.\n";
+		current_vm()->factorbug();
 	}
 	last_nano_count = nanos;
 	ctx->push(from_unsigned_8(nanos));
