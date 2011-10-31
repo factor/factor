e10c3183c8d5865b3a83ff3c86f91d54aa5adbdc
Joe Groff <arcata@gmail.com>
2011-10-30 22:30:55 -0700

    vm: add primitives to lift sample data to factor

--- vm/vm.hpp
+++ vm/vm.hpp
@@ -198,6 +198,8 @@ struct factor_vm
 	void end_sampling_profiler();
 	void set_sampling_profiler(bool sampling);
 	void primitive_sampling_profiler();
+	void primitive_get_samples();
+	void primitive_clear_samples();
 
 	// errors
 	void general_error(vm_error_type error, cell arg1, cell arg2);
