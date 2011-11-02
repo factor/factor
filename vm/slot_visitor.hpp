2a64c3ed696a8acca1fe22de0a38b359444a40d7
Joe Groff <arcata@gmail.com>
2011-11-01 18:53:37 -0700

    vm: sample code block owners instead of blocks

--- vm/slot_visitor.hpp
+++ vm/slot_visitor.hpp
@@ -134,6 +134,7 @@ template<typename Fixup> struct slot_visitor {
 	void visit_contexts();
 	void visit_code_block_objects(code_block *compiled);
 	void visit_embedded_literals(code_block *compiled);
+	void visit_sample_callstacks();
 };
 
 template<typename Fixup>
@@ -250,6 +251,17 @@ void slot_visitor<Fixup>::visit_literal_table_roots()
 }
 
 template<typename Fixup>
+void slot_visitor<Fixup>::visit_sample_callstacks()
+{
+	for (std::vector<cell>::iterator iter = parent->sample_callstacks.begin();
+		iter != parent->sample_callstacks.end();
+		++iter)
+	{
+		visit_handle(&*iter);
+	}
+}
+
+template<typename Fixup>
 void slot_visitor<Fixup>::visit_roots()
 {
 	visit_handle(&parent->true_object);
@@ -261,6 +273,7 @@ void slot_visitor<Fixup>::visit_roots()
 	visit_bignum_roots();
 	visit_callback_roots();
 	visit_literal_table_roots();
+	visit_sample_callstacks();
 
 	visit_object_array(parent->special_objects,parent->special_objects + special_object_count);
 }
