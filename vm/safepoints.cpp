d3f32d396472037ee963466783ea9a89923f25b6
Joe Groff <arcata@gmail.com>
2011-11-07 18:53:07 -0800

    vm: write signals to pipe immediately The whole point is to wake up the mx without waiting for the next safepoint derp

--- vm/safepoints.cpp
+++ vm/safepoints.cpp
@@ -8,19 +8,11 @@ void safepoint_state::enqueue_safepoint() volatile
 	parent->code->guard_safepoint();
 }
 
-void safepoint_state::enqueue_fep(cell signal) volatile
+void safepoint_state::enqueue_fep() volatile
 {
 	if (parent->fep_p)
 		fatal_error("Low-level debugger interrupted", 0);
 	atomic::store(&fep_p, true);
-	if (signal != 0)
-		atomic::store(&queued_signal, signal);
-	enqueue_safepoint();
-}
-
-void safepoint_state::enqueue_signal(cell signal) volatile
-{
-	atomic::store(&queued_signal, signal);
 	enqueue_safepoint();
 }
 
@@ -28,9 +20,9 @@ void safepoint_state::enqueue_samples(cell samples, cell pc, bool foreign_thread
 {
 	if (atomic::load(&parent->sampling_profiler_p))
 	{
-		atomic::add(&sample_counts.sample_count, samples);
+		atomic::fetch_add(&sample_counts.sample_count, samples);
 		if (foreign_thread_p)
-			atomic::add(&sample_counts.foreign_thread_sample_count, samples);
+			atomic::fetch_add(&sample_counts.foreign_thread_sample_count, samples);
 		else {
 			if (atomic::load(&parent->current_gc_p))
 				atomic::fetch_add(&sample_counts.gc_sample_count, samples);
@@ -46,8 +38,6 @@ void safepoint_state::enqueue_samples(cell samples, cell pc, bool foreign_thread
 void safepoint_state::handle_safepoint() volatile
 {
 	parent->code->unguard_safepoint();
-
-	report_signal(parent->signal_pipe_input);
 
 	if (atomic::load(&fep_p))
 	{
