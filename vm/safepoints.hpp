04c6b2c393ffc2fda27a1d3ddf4655d17cf95cc1
Joe Groff <arcata@gmail.com>
2011-11-07 18:53:07 -0800

    vm: write signals to pipe immediately The whole point is to wake up the mx without waiting for the next safepoint derp

--- vm/safepoints.hpp
+++ vm/safepoints.hpp
@@ -6,13 +6,11 @@ struct safepoint_state
 	factor_vm *parent;
 
 	cell fep_p;
-	cell queued_signal;
 	profiling_sample_count sample_counts;
 
 	safepoint_state(factor_vm *parent) :
 		parent(parent),
 		fep_p(false),
-		queued_signal(0),
 		sample_counts()
 	{
 	}
@@ -21,12 +19,7 @@ struct safepoint_state
 
 	void enqueue_safepoint() volatile;
 	void enqueue_samples(cell samples, cell pc, bool foreign_thread_p) volatile;
-	void enqueue_fep(cell signal) volatile;
-
-	// os-*.cpp
-	void enqueue_signal(cell signal) volatile;
-	void report_signal(int fd) volatile;
-
+	void enqueue_fep() volatile;
 };
 
 }
