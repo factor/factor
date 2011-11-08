fca09f21ed557bc83904879c5ea2b89019e34b80
Joe Groff <arcata@gmail.com>
2011-11-08 11:06:00 -0800

    vm: don't pass this to safepoint constructor It's bad juju and MSVC calls us out on it.

--- vm/safepoints.hpp
+++ vm/safepoints.hpp
@@ -3,23 +3,20 @@ namespace factor
 
 struct safepoint_state
 {
-	factor_vm *parent;
-
 	cell fep_p;
 	profiling_sample_count sample_counts;
 
-	safepoint_state(factor_vm *parent) :
-		parent(parent),
+	safepoint_state() :
 		fep_p(false),
 		sample_counts()
 	{
 	}
 
-	void handle_safepoint() volatile;
+	void handle_safepoint(factor_vm *parent) volatile;
 
-	void enqueue_safepoint() volatile;
-	void enqueue_samples(cell samples, cell pc, bool foreign_thread_p) volatile;
-	void enqueue_fep() volatile;
+	void enqueue_safepoint(factor_vm *parent) volatile;
+	void enqueue_samples(factor_vm *parent, cell samples, cell pc, bool foreign_thread_p) volatile;
+	void enqueue_fep(factor_vm *parent) volatile;
 };
 
 }
