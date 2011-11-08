e4c3722667d5247edac98ef717b6323ecdb625fd
Joe Groff <arcata@gmail.com>
2011-11-07 21:12:11 -0800

    vm: interrupt stdin_loop when entering fep The stdin_loop thread will keep trying to consume input unless we stop it by sending it a signal. Use SIGUSR2 to stop the read syscall and a mutex to hold up the loop while the fep is active.

--- vm/os-windows.cpp
+++ vm/os-windows.cpp
@@ -301,6 +301,14 @@ void factor_vm::open_console()
 	SetConsoleCtrlHandler(factor::ctrl_handler, TRUE);
 }
 
+void factor_vm::lock_console()
+{
+}
+
+void factor_vm::unlock_console()
+{
+}
+
 void factor_vm::sampler_thread_loop()
 {
 	LARGE_INTEGER counter, new_counter, units_per_second;
