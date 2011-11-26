af8821dd1df2df6e4ca465fa0958202b05b3d3f4
Joe Groff <arcata@gmail.com>
2011-11-25 19:23:07 -0800

    vm: pack frame size into code block header bits

--- vm/code_blocks.hpp
+++ vm/code_blocks.hpp
@@ -5,15 +5,17 @@ namespace factor
 struct code_block
 {
 	// header format (bits indexed with least significant as zero):
-	// bit   0   : free?
-	// bits  1- 2: type (as a code_block_type)
-	// bits  4-  : code size / 16
+	// bit   0  : free?
+	// bits  1-2: type (as a code_block_type)
+	// if not free:
+	//   bits  3-23: code size / 8
+	//   bits 24-31: stack frame size / 16
+	// if free:
+	//   bits  3-end: code size / 8
 	cell header;
 	cell owner; /* tagged pointer to word, quotation or f */
 	cell parameters; /* tagged pointer to array or f */
 	cell relocation; /* tagged pointer to byte-array or f */
-	cell stack_frame_size;
-	cell pad;
 
 	bool free_p() const
 	{
@@ -42,9 +44,30 @@ struct code_block
 
 	cell size() const
 	{
-		cell size = header & ~7;
+		cell size;
+		if (free_p())
+			size = header & ~7;
+		else
+			size = header & 0xFFFFF8;
 		FACTOR_ASSERT(size > 0);
 		return size;
+	}
+
+	cell stack_frame_size() const
+	{
+		if (free_p())
+			return 0;
+		else
+			return (header >> 20) & 0xFF0;
+	}
+
+	void set_stack_frame_size(cell frame_size)
+	{
+		FACTOR_ASSERT(size() < 0xFFFFFF);
+		FACTOR_ASSERT(!free_p());
+		FACTOR_ASSERT(frame_size % 16 == 0);
+		FACTOR_ASSERT(frame_size <= 0xFF0);
+		header = (header & 0xFFFFFF) | (frame_size << 20);
 	}
 
 	template<typename Fixup> cell size(Fixup fixup) const
