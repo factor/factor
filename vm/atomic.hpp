6dacc4402961bc8d9b229d2d9975cd223bf957f1
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 21:43:20 -0400

    VM: Refactor atomic.hpp to Factor style

--- vm/atomic.hpp
+++ vm/atomic.hpp
@@ -1,27 +1,23 @@
 namespace factor {
-	namespace atomic {
-		FACTOR_FORCE_INLINE static cell load(volatile cell *ptr)
-		{
-			atomic::fence();
-			return *ptr;
-		}
+namespace atomic {
+FACTOR_FORCE_INLINE static cell load(volatile cell* ptr) {
+  atomic::fence();
+  return *ptr;
+}
 
-		FACTOR_FORCE_INLINE static fixnum load(volatile fixnum *ptr)
-		{
-			atomic::fence();
-			return *ptr;
-		}
+FACTOR_FORCE_INLINE static fixnum load(volatile fixnum* ptr) {
+  atomic::fence();
+  return *ptr;
+}
 
-		FACTOR_FORCE_INLINE static void store(volatile cell *ptr, cell val)
-		{
-			*ptr = val;
-			atomic::fence();
-		}
+FACTOR_FORCE_INLINE static void store(volatile cell* ptr, cell val) {
+  *ptr = val;
+  atomic::fence();
+}
 
-		FACTOR_FORCE_INLINE static void store(volatile fixnum *ptr, fixnum val)
-		{
-			*ptr = val;
-			atomic::fence();
-		}
-	}
+FACTOR_FORCE_INLINE static void store(volatile fixnum* ptr, fixnum val) {
+  *ptr = val;
+  atomic::fence();
+}
+}
 }
