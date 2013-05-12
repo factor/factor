6c919e7d72522fe8e9722aea721f2bc49ca97571
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 21:40:14 -0400

    VM: Refactor aging_space to Factor style

--- vm/aging_space.hpp
+++ vm/aging_space.hpp
@@ -1,20 +1,19 @@
-namespace factor
-{
+namespace factor {
 
 struct aging_space : bump_allocator<object> {
-	object_start_map starts;
+  object_start_map starts;
 
-	explicit aging_space(cell size, cell start) :
-		bump_allocator<object>(size,start), starts(size,start) {}
+  explicit aging_space(cell size, cell start)
+      : bump_allocator<object>(size, start), starts(size, start) {}
 
-	object *allot(cell size)
-	{
-		if(here + size > end) return NULL;
+  object* allot(cell size) {
+    if (here + size > end)
+      return NULL;
 
-		object *obj = bump_allocator<object>::allot(size);
-		starts.record_object_start_offset(obj);
-		return obj;
-	}
+    object* obj = bump_allocator<object>::allot(size);
+    starts.record_object_start_offset(obj);
+    return obj;
+  }
 };
 
 }
