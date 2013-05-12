92e9963694bc35545efa6cb048f91a77d09294c5
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:29:54 -0400

    VM: Refactor to_tenured_collector.cpp/hpp to Factor style

--- vm/to_tenured_collector.hpp
+++ vm/to_tenured_collector.hpp
@@ -1,28 +1,26 @@
-namespace factor
-{
+namespace factor {
 
 struct to_tenured_policy {
-	factor_vm *parent;
-	tenured_space *tenured;
+  factor_vm* parent;
+  tenured_space* tenured;
 
-	explicit to_tenured_policy(factor_vm *parent_) : parent(parent_), tenured(parent->data->tenured) {}
+  explicit to_tenured_policy(factor_vm* parent_)
+      : parent(parent_), tenured(parent->data->tenured) {}
 
-	bool should_copy_p(object *untagged)
-	{
-		return !tenured->contains_p(untagged);
-	}
+  bool should_copy_p(object* untagged) {
+    return !tenured->contains_p(untagged);
+  }
 
-	void promoted_object(object *obj)
-	{
-		parent->mark_stack.push_back((cell)obj);
-	}
+  void promoted_object(object* obj) {
+    parent->mark_stack.push_back((cell) obj);
+  }
 
-	void visited_object(object *obj) {}
+  void visited_object(object* obj) {}
 };
 
-struct to_tenured_collector : collector<tenured_space,to_tenured_policy> {
-	explicit to_tenured_collector(factor_vm *parent_);
-	void tenure_reachable_objects();
+struct to_tenured_collector : collector<tenured_space, to_tenured_policy> {
+  explicit to_tenured_collector(factor_vm* parent_);
+  void tenure_reachable_objects();
 };
 
 }
