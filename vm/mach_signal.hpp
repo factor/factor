07965c4fe8f12c091a7fe5203ca8b654455bb3ca
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:11:09 -0400

    VM: Refactor mach_signal to Factor style

--- vm/mach_signal.hpp
+++ vm/mach_signal.hpp
@@ -36,48 +36,28 @@ Modified for Factor by Slava Pestov */
    allowing the thread to continue from the point of the exception; otherwise,
    no reply message is sent and the called routine must have dealt with the
    exception thread directly.  */
-extern "C" boolean_t exc_server (mach_msg_header_t *request_msg, mach_msg_header_t *reply_msg);
+extern "C" boolean_t exc_server(mach_msg_header_t* request_msg,
+                                mach_msg_header_t* reply_msg);
 
 /* http://web.mit.edu/darwin/src/modules/xnu/osfmk/man/catch_exception_raise.html
    These functions are defined in this file, and called by exc_server.
    FIXME: What needs to be done when this code is put into a shared library? */
-extern "C"
-kern_return_t
-catch_exception_raise (mach_port_t exception_port,
-		       mach_port_t thread,
-		       mach_port_t task,
-		       exception_type_t exception,
-		       exception_data_t code,
-		       mach_msg_type_number_t code_count);
-extern "C"
-kern_return_t
-catch_exception_raise_state (mach_port_t exception_port,
-			     exception_type_t exception,
-			     exception_data_t code,
-			     mach_msg_type_number_t code_count,
-			     thread_state_flavor_t *flavor,
-			     thread_state_t in_state,
-			     mach_msg_type_number_t in_state_count,
-			     thread_state_t out_state,
-			     mach_msg_type_number_t *out_state_count);
+extern "C" kern_return_t catch_exception_raise(
+    mach_port_t exception_port, mach_port_t thread, mach_port_t task,
+    exception_type_t exception, exception_data_t code,
+    mach_msg_type_number_t code_count);
+extern "C" kern_return_t catch_exception_raise_state(
+    mach_port_t exception_port, exception_type_t exception,
+    exception_data_t code, mach_msg_type_number_t code_count,
+    thread_state_flavor_t* flavor, thread_state_t in_state,
+    mach_msg_type_number_t in_state_count, thread_state_t out_state,
+    mach_msg_type_number_t* out_state_count);
 
-extern "C"
-kern_return_t
-catch_exception_raise_state_identity (mach_port_t exception_port,
-				      mach_port_t thread,
-				      mach_port_t task,
-				      exception_type_t exception,
-				      exception_data_t code,
-				      mach_msg_type_number_t codeCnt,
-				      thread_state_flavor_t *flavor,
-				      thread_state_t in_state,
-				      mach_msg_type_number_t in_state_count,
-				      thread_state_t out_state,
-				      mach_msg_type_number_t *out_state_count);
+extern "C" kern_return_t catch_exception_raise_state_identity(
+    mach_port_t exception_port, mach_port_t thread, mach_port_t task,
+    exception_type_t exception, exception_data_t code,
+    mach_msg_type_number_t codeCnt, thread_state_flavor_t* flavor,
+    thread_state_t in_state, mach_msg_type_number_t in_state_count,
+    thread_state_t out_state, mach_msg_type_number_t* out_state_count);
 
-namespace factor
-{
-
-void mach_initialize ();
-
-}
+namespace factor { void mach_initialize(); }
