d57c1cc579cd354a895cafbaebf174a5ed9040fe
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:14:17 -0400

    VM: Refactor mvm* to Factor style

--- vm/mvm-unix.cpp
+++ vm/mvm-unix.cpp
@@ -1,24 +1,20 @@
 #include "master.hpp"
 
-namespace factor
-{
+namespace factor {
 
 pthread_key_t current_vm_tls_key;
 
-void init_mvm()
-{
-	if(pthread_key_create(&current_vm_tls_key, NULL) != 0)
-		fatal_error("pthread_key_create() failed",0);
+void init_mvm() {
+  if (pthread_key_create(&current_vm_tls_key, NULL) != 0)
+    fatal_error("pthread_key_create() failed", 0);
 }
 
-void register_vm_with_thread(factor_vm *vm)
-{
-	pthread_setspecific(current_vm_tls_key,vm);
+void register_vm_with_thread(factor_vm* vm) {
+  pthread_setspecific(current_vm_tls_key, vm);
 }
 
-factor_vm *current_vm_p()
-{
-	return (factor_vm*)pthread_getspecific(current_vm_tls_key);
+factor_vm* current_vm_p() {
+  return (factor_vm*)pthread_getspecific(current_vm_tls_key);
 }
 
 }
