d718a091f45dd85dcf4828e201db8c5f78daae81
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:32:45 -0400

    VM: Refactor allot.hpp to Factor style

--- vm/allot.hpp
+++ vm/allot.hpp
@@ -1,34 +1,30 @@
-namespace factor
-{
+namespace factor {
 
 /*
  * It is up to the caller to fill in the object's fields in a meaningful
  * fashion!
  */
 /* Allocates memory */
-inline object *factor_vm::allot_object(cell type, cell size)
-{
+inline object* factor_vm::allot_object(cell type, cell size) {
 #ifdef FACTOR_DEBUG
-	FACTOR_ASSERT(!current_gc);
+  FACTOR_ASSERT(!current_gc);
 #endif
 
-	/* If the object is smaller than the nursery, allocate it in the nursery,
-	after a GC if needed */
-	if(nursery.size > size)
-	{
-		/* If there is insufficient room, collect the nursery */
-		if(nursery.here + size > nursery.end)
-			primitive_minor_gc();
+  /* If the object is smaller than the nursery, allocate it in the nursery,
+     after a GC if needed */
+  if (nursery.size > size) {
+    /* If there is insufficient room, collect the nursery */
+    if (nursery.here + size > nursery.end)
+      primitive_minor_gc();
 
-		object *obj = nursery.allot(size);
+    object* obj = nursery.allot(size);
 
-		obj->initialize(type);
-		return obj;
-	}
-	/* If the object is bigger than the nursery, allocate it in
-	tenured space */
-	else
-		return allot_large_object(type,size);
+    obj->initialize(type);
+    return obj;
+  } /* If the object is bigger than the nursery, allocate it in
+       tenured space */
+  else
+    return allot_large_object(type, size);
 }
 
 }
