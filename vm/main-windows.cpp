19348c5529879390a8027730a78663424f269847
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:12:16 -0400

    VM: Refactor main-* to Factor style

--- vm/main-windows.cpp
+++ vm/main-windows.cpp
@@ -1,21 +1,16 @@
 #include "master.hpp"
 
-VM_C_API int wmain(int argc, wchar_t **argv)
-{
-	factor::init_globals();
-	factor::start_standalone_factor(argc,argv);
-	return 0;
+VM_C_API int wmain(int argc, wchar_t** argv) {
+  factor::init_globals();
+  factor::start_standalone_factor(argc, argv);
+  return 0;
 }
 
-int WINAPI WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow)
-{
-	int argc;
-	wchar_t **argv = CommandLineToArgvW(GetCommandLine(),&argc);
-	wmain(argc,argv);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow) {
+  int argc;
+  wchar_t** argv = CommandLineToArgvW(GetCommandLine(), &argc);
+  wmain(argc, argv);
 
-	return 0;
+  return 0;
 }
