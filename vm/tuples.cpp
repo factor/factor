4754fa3889b05f4c0bcc0cbefd40319b4c194bd8
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:30:22 -0400

    VM: Refactor tuples.cpp/hpp to Factor style

--- vm/tuples.cpp
+++ vm/tuples.cpp
@@ -1,34 +1,32 @@
 #include "master.hpp"
 
-namespace factor
-{
+namespace factor {
 
 /* push a new tuple on the stack, filling its slots with f */
 /* Allocates memory */
-void factor_vm::primitive_tuple()
-{
-	data_root<tuple_layout> layout(ctx->pop(),this);
-	tagged<tuple> t(allot<tuple>(tuple_size(layout.untagged())));
-	t->layout = layout.value();
+void factor_vm::primitive_tuple() {
+  data_root<tuple_layout> layout(ctx->pop(), this);
+  tagged<tuple> t(allot<tuple>(tuple_size(layout.untagged())));
+  t->layout = layout.value();
 
-	memset_cell(t->data(),false_object,tuple_size(layout.untagged()) - sizeof(cell));
+  memset_cell(t->data(), false_object,
+              tuple_size(layout.untagged()) - sizeof(cell));
 
-	ctx->push(t.value());
+  ctx->push(t.value());
 }
 
 /* push a new tuple on the stack, filling its slots from the stack */
 /* Allocates memory */
-void factor_vm::primitive_tuple_boa()
-{
-	data_root<tuple_layout> layout(ctx->pop(),this);
-	tagged<tuple> t(allot<tuple>(tuple_size(layout.untagged())));
-	t->layout = layout.value();
+void factor_vm::primitive_tuple_boa() {
+  data_root<tuple_layout> layout(ctx->pop(), this);
+  tagged<tuple> t(allot<tuple>(tuple_size(layout.untagged())));
+  t->layout = layout.value();
 
-	cell size = untag_fixnum(layout.untagged()->size) * sizeof(cell);
-	memcpy(t->data(),(cell *)(ctx->datastack - size + sizeof(cell)),size);
-	ctx->datastack -= size;
+  cell size = untag_fixnum(layout.untagged()->size) * sizeof(cell);
+  memcpy(t->data(), (cell*)(ctx->datastack - size + sizeof(cell)), size);
+  ctx->datastack -= size;
 
-	ctx->push(t.value());
+  ctx->push(t.value());
 }
 
 }
