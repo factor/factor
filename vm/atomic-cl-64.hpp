ca1276841dbd5c755311b5413ef59e587a22e116
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 21:43:04 -0400

    VM: Refactor atomic-* to Factor style

--- vm/atomic-cl-64.hpp
+++ vm/atomic-cl-64.hpp
@@ -1,49 +1,39 @@
 #define FACTOR_FORCE_INLINE __forceinline
 
 namespace factor {
-	namespace atomic {
-		__forceinline static bool cas(volatile cell *ptr, cell old_val, cell new_val)
-		{
-			return InterlockedCompareExchange64(
-				reinterpret_cast<volatile LONG64 *>(ptr),
-				(LONG64)old_val,
-				(LONG64)new_val) == (LONG64)old_val;
-		}
-		__forceinline static bool cas(volatile fixnum *ptr, fixnum old_val, fixnum new_val)
-		{
-			return InterlockedCompareExchange64(
-				reinterpret_cast<volatile LONG64 *>(ptr),
-				(LONG64)old_val,
-				(LONG64)new_val) == (LONG64)old_val;
-		}
+namespace atomic {
+__forceinline static bool cas(volatile cell* ptr, cell old_val, cell new_val) {
+  return InterlockedCompareExchange64(reinterpret_cast<volatile LONG64*>(ptr),
+                                      (LONG64) old_val, (LONG64) new_val) ==
+         (LONG64) old_val;
+}
+__forceinline static bool cas(volatile fixnum* ptr, fixnum old_val,
+                              fixnum new_val) {
+  return InterlockedCompareExchange64(reinterpret_cast<volatile LONG64*>(ptr),
+                                      (LONG64) old_val, (LONG64) new_val) ==
+         (LONG64) old_val;
+}
 
-		__forceinline static cell fetch_add(volatile cell *ptr, cell val)
-		{
-			return (cell)InterlockedExchangeAdd64(
-				reinterpret_cast<volatile LONG64 *>(ptr), (LONG64)val);
-		}
-		__forceinline static fixnum fetch_add(volatile fixnum *ptr, fixnum val)
-		{
-			return (fixnum)InterlockedExchangeAdd64(
-				reinterpret_cast<volatile LONG64 *>(ptr), (LONG64)val);
-		}
+__forceinline static cell fetch_add(volatile cell* ptr, cell val) {
+  return (cell) InterlockedExchangeAdd64(
+      reinterpret_cast<volatile LONG64*>(ptr), (LONG64) val);
+}
+__forceinline static fixnum fetch_add(volatile fixnum* ptr, fixnum val) {
+  return (fixnum) InterlockedExchangeAdd64(
+      reinterpret_cast<volatile LONG64*>(ptr), (LONG64) val);
+}
 
-		__forceinline static cell fetch_subtract(volatile cell *ptr, cell val)
-		{
-			return (cell)InterlockedExchangeAdd64(
-				reinterpret_cast<volatile LONG64 *>(ptr), -(LONG64)val);
-		}
-		__forceinline static fixnum fetch_subtract(volatile fixnum *ptr, fixnum val)
-		{
-			return (fixnum)InterlockedExchangeAdd64(
-				reinterpret_cast<volatile LONG64 *>(ptr), -(LONG64)val);
-		}
+__forceinline static cell fetch_subtract(volatile cell* ptr, cell val) {
+  return (cell) InterlockedExchangeAdd64(
+      reinterpret_cast<volatile LONG64*>(ptr), -(LONG64) val);
+}
+__forceinline static fixnum fetch_subtract(volatile fixnum* ptr, fixnum val) {
+  return (fixnum) InterlockedExchangeAdd64(
+      reinterpret_cast<volatile LONG64*>(ptr), -(LONG64) val);
+}
 
-		__forceinline static void fence()
-		{
-			MemoryBarrier();
-		}
-	}
+__forceinline static void fence() { MemoryBarrier(); }
+}
 }
 
 #include "atomic.hpp"
