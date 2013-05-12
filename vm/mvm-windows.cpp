00002ba245f05cdfa5033a1df26577890ee046c2
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:14:17 -0400

    VM: Refactor mvm* to Factor style

--- vm/mvm-windows.cpp
+++ vm/mvm-windows.cpp
@@ -1,25 +1,21 @@
 #include "master.hpp"
 
-namespace factor
-{
+namespace factor {
 
-DWORD current_vm_tls_key; 
+DWORD current_vm_tls_key;
 
-void init_mvm()
-{
-	if((current_vm_tls_key = TlsAlloc()) == TLS_OUT_OF_INDEXES)
-		fatal_error("TlsAlloc() failed",0);
+void init_mvm() {
+  if ((current_vm_tls_key = TlsAlloc()) == TLS_OUT_OF_INDEXES)
+    fatal_error("TlsAlloc() failed", 0);
 }
 
-void register_vm_with_thread(factor_vm *vm)
-{
-	if(!TlsSetValue(current_vm_tls_key, vm))
-		fatal_error("TlsSetValue() failed",0);
+void register_vm_with_thread(factor_vm* vm) {
+  if (!TlsSetValue(current_vm_tls_key, vm))
+    fatal_error("TlsSetValue() failed", 0);
 }
 
-factor_vm *current_vm_p()
-{
-	return (factor_vm *)TlsGetValue(current_vm_tls_key);
+factor_vm* current_vm_p() {
+  return (factor_vm*)TlsGetValue(current_vm_tls_key);
 }
 
 }
--- vm/mvm.cpp
+++ vm/mvm.cpp
@@ -1,31 +1,31 @@
 #include "master.hpp"
 
-namespace factor
-{
+namespace factor {
 
 std::map<THREADHANDLE, factor_vm*> thread_vms;
 
 struct startargs {
-	int argc;
-	vm_char **argv;
+  int argc;
+  vm_char** argv;
 };
 
 // arg must be new'ed because we're going to delete it!
-void *start_standalone_factor_thread(void *arg) 
-{
-	factor_vm *newvm = new_factor_vm();
-	startargs *args = (startargs*) arg;
-	int argc = args->argc; vm_char **argv = args->argv;
-	delete args;
-	newvm->start_standalone_factor(argc, argv);
-	return 0;
+void* start_standalone_factor_thread(void* arg) {
+  factor_vm* newvm = new_factor_vm();
+  startargs* args = (startargs*)arg;
+  int argc = args->argc;
+  vm_char** argv = args->argv;
+  delete args;
+  newvm->start_standalone_factor(argc, argv);
+  return 0;
 }
 
-VM_C_API THREADHANDLE start_standalone_factor_in_new_thread(int argc, vm_char **argv)
-{
-	startargs *args = new startargs;
-	args->argc = argc; args->argv = argv; 
-	return start_thread(start_standalone_factor_thread,args);
+VM_C_API THREADHANDLE start_standalone_factor_in_new_thread(int argc,
+                                                            vm_char** argv) {
+  startargs* args = new startargs;
+  args->argc = argc;
+  args->argv = argv;
+  return start_thread(start_standalone_factor_thread, args);
 }
 
 }
