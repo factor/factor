9ef732743dcff6d7f2f0ca74e233288e6287963d
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 21:42:22 -0400

    VM: Refactor assert.hpp to Factor style

--- vm/assert.hpp
+++ vm/assert.hpp
@@ -1,16 +1,12 @@
-namespace factor
-{
-    void abort();
-}
+namespace factor { void abort(); }
 
 #ifdef FACTOR_DEBUG
-#define FACTOR_ASSERT(condition) ((condition) \
-    ? (void)0 \
-    : ( \
-        ::fprintf(stderr, "assertion \"%s\" failed: file \"%s\", line %d\n", \
-            #condition, __FILE__, __LINE__), \
-        ::factor::abort() \
-    ))
+#define FACTOR_ASSERT(condition)                                               \
+  ((condition)                                                                 \
+       ? (void) 0                                                              \
+       : (::fprintf(stderr, "assertion \"%s\" failed: file \"%s\", line %d\n", \
+                    #condition, __FILE__, __LINE__),                           \
+          ::factor::abort()))
 #else
-#define FACTOR_ASSERT(condition) ((void)0)
+#define FACTOR_ASSERT(condition) ((void) 0)
 #endif
