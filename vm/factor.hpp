f5d484b21bfee62b56696c85f208eff1140e64e3
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 21:59:58 -0400

    VM: Refactor factor.cpp/hpp to Factor style

--- vm/factor.hpp
+++ vm/factor.hpp
@@ -1,8 +1,7 @@
-namespace factor
-{
+namespace factor {
 
 VM_C_API void init_globals();
-factor_vm *new_factor_vm();
-VM_C_API void start_standalone_factor(int argc, vm_char **argv);
+factor_vm* new_factor_vm();
+VM_C_API void start_standalone_factor(int argc, vm_char** argv);
 
 }
