ee9fd64b69ad38768cc0b4b57ff9ac80ebb511e7
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 21:39:48 -0400

    VM: Refactor aging_collector to Factor style

--- vm/aging_collector.hpp
+++ vm/aging_collector.hpp
@@ -1,28 +1,26 @@
-namespace factor
-{
+namespace factor {
 
 struct aging_policy {
-	factor_vm *parent;
-	aging_space *aging;
-	tenured_space *tenured;
+  factor_vm* parent;
+  aging_space* aging;
+  tenured_space* tenured;
 
-	explicit aging_policy(factor_vm *parent_) :
-		parent(parent_),
-		aging(parent->data->aging),
-		tenured(parent->data->tenured) {}
+  explicit aging_policy(factor_vm* parent_)
+      : parent(parent_),
+        aging(parent->data->aging),
+        tenured(parent->data->tenured) {}
 
-	bool should_copy_p(object *untagged)
-	{
-		return !(aging->contains_p(untagged) || tenured->contains_p(untagged));
-	}
+  bool should_copy_p(object* untagged) {
+    return !(aging->contains_p(untagged) || tenured->contains_p(untagged));
+  }
 
-	void promoted_object(object *obj) {}
+  void promoted_object(object* obj) {}
 
-	void visited_object(object *obj) {}
+  void visited_object(object* obj) {}
 };
 
-struct aging_collector : copying_collector<aging_space,aging_policy> {
-	explicit aging_collector(factor_vm *parent_);
+struct aging_collector : copying_collector<aging_space, aging_policy> {
+  explicit aging_collector(factor_vm* parent_);
 };
 
 }
