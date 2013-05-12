fd6ad22ed7a4ef9edd3f5e33bb7e50657172c0b9
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 21:53:30 -0400

    VM: Refactor copying_collector.hpp to Factor style

--- vm/copying_collector.hpp
+++ vm/copying_collector.hpp
@@ -1,21 +1,20 @@
-namespace factor
-{
+namespace factor {
 
-template<typename TargetGeneration, typename Policy>
-struct copying_collector : collector<TargetGeneration,Policy> {
-	cell scan;
+template <typename TargetGeneration, typename Policy>
+struct copying_collector : collector<TargetGeneration, Policy> {
+  cell scan;
 
-	explicit copying_collector(factor_vm *parent_, TargetGeneration *target_, Policy policy_) :
-		collector<TargetGeneration,Policy>(parent_,target_,policy_), scan(target_->here) {}
+  explicit copying_collector(factor_vm* parent_, TargetGeneration* target_,
+                             Policy policy_)
+      : collector<TargetGeneration, Policy>(parent_, target_, policy_),
+        scan(target_->here) {}
 
-	void cheneys_algorithm()
-	{
-		while(scan && scan < this->target->here)
-		{
-			this->trace_object((object *)scan);
-			scan = this->target->next_object_after(scan);
-		}
-	}
+  void cheneys_algorithm() {
+    while (scan && scan < this->target->here) {
+      this->trace_object((object*)scan);
+      scan = this->target->next_object_after(scan);
+    }
+  }
 };
 
 }
