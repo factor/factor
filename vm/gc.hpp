558bd5af1c458e44781eb82ad6490653fa485241
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:04:08 -0400

    VM: Refactor gc* to Factor style

--- vm/gc.hpp
+++ vm/gc.hpp
@@ -1,55 +1,54 @@
-namespace factor
-{
+namespace factor {
 
 enum gc_op {
-	collect_nursery_op,
-	collect_aging_op,
-	collect_to_tenured_op,
-	collect_full_op,
-	collect_compact_op,
-	collect_growing_heap_op
+  collect_nursery_op,
+  collect_aging_op,
+  collect_to_tenured_op,
+  collect_full_op,
+  collect_compact_op,
+  collect_growing_heap_op
 };
 
 struct gc_event {
-	gc_op op;
-	data_heap_room data_heap_before;
-	code_heap_room code_heap_before;
-	data_heap_room data_heap_after;
-	code_heap_room code_heap_after;
-	cell cards_scanned;
-	cell decks_scanned;
-	cell code_blocks_scanned;
-	u64 start_time;
-	cell total_time;
-	cell card_scan_time;
-	cell code_scan_time;
-	cell data_sweep_time;
-	cell code_sweep_time;
-	cell compaction_time;
-	u64 temp_time;
+  gc_op op;
+  data_heap_room data_heap_before;
+  code_heap_room code_heap_before;
+  data_heap_room data_heap_after;
+  code_heap_room code_heap_after;
+  cell cards_scanned;
+  cell decks_scanned;
+  cell code_blocks_scanned;
+  u64 start_time;
+  cell total_time;
+  cell card_scan_time;
+  cell code_scan_time;
+  cell data_sweep_time;
+  cell code_sweep_time;
+  cell compaction_time;
+  u64 temp_time;
 
-	gc_event(gc_op op_, factor_vm *parent);
-	void started_card_scan();
-	void ended_card_scan(cell cards_scanned_, cell decks_scanned_);
-	void started_code_scan();
-	void ended_code_scan(cell code_blocks_scanned_);
-	void started_data_sweep();
-	void ended_data_sweep();
-	void started_code_sweep();
-	void ended_code_sweep();
-	void started_compaction();
-	void ended_compaction();
-	void ended_gc(factor_vm *parent);
+  gc_event(gc_op op_, factor_vm* parent);
+  void started_card_scan();
+  void ended_card_scan(cell cards_scanned_, cell decks_scanned_);
+  void started_code_scan();
+  void ended_code_scan(cell code_blocks_scanned_);
+  void started_data_sweep();
+  void ended_data_sweep();
+  void started_code_sweep();
+  void ended_code_sweep();
+  void started_compaction();
+  void ended_compaction();
+  void ended_gc(factor_vm* parent);
 };
 
 struct gc_state {
-	gc_op op;
-	u64 start_time;
-	gc_event *event;
+  gc_op op;
+  u64 start_time;
+  gc_event* event;
 
-	explicit gc_state(gc_op op_, factor_vm *parent);
-	~gc_state();
-	void start_again(gc_op op_, factor_vm *parent);
+  explicit gc_state(gc_op op_, factor_vm* parent);
+  ~gc_state();
+  void start_again(gc_op op_, factor_vm* parent);
 };
 
 }
--- vm/gc_info.hpp
+++ vm/gc_info.hpp
@@ -1,67 +1,48 @@
-namespace factor
-{
+namespace factor {
 
 struct gc_info {
-	u32 scrub_d_count;
-	u32 scrub_r_count;
-	u32 gc_root_count;
-	u32 derived_root_count;
-	u32 return_address_count;
+  u32 scrub_d_count;
+  u32 scrub_r_count;
+  u32 gc_root_count;
+  u32 derived_root_count;
+  u32 return_address_count;
 
-	cell callsite_bitmap_size()
-	{
-		return scrub_d_count + scrub_r_count + gc_root_count;
-	}
+  cell callsite_bitmap_size() {
+    return scrub_d_count + scrub_r_count + gc_root_count;
+  }
 
-	cell total_bitmap_size()
-	{
-		return return_address_count * callsite_bitmap_size();
-	}
+  cell total_bitmap_size() {
+    return return_address_count * callsite_bitmap_size();
+  }
 
-	cell total_bitmap_bytes()
-	{
-		return ((total_bitmap_size() + 7) / 8);
-	}
+  cell total_bitmap_bytes() { return ((total_bitmap_size() + 7) / 8); }
 
-	u32 *return_addresses()
-	{
-		return (u32 *)this - return_address_count;
-	}
+  u32* return_addresses() { return (u32*)this - return_address_count; }
 
-	u32 *base_pointer_map()
-	{
-		return return_addresses() - return_address_count * derived_root_count;
-	}
+  u32* base_pointer_map() {
+    return return_addresses() - return_address_count * derived_root_count;
+  }
 
-	u8 *gc_info_bitmap()
-	{
-		return (u8 *)base_pointer_map() - total_bitmap_bytes();
-	}
+  u8* gc_info_bitmap() {
+    return (u8*)base_pointer_map() - total_bitmap_bytes();
+  }
 
-	cell callsite_scrub_d(cell index)
-	{
-		return index * scrub_d_count;
-	}
+  cell callsite_scrub_d(cell index) { return index * scrub_d_count; }
 
-	cell callsite_scrub_r(cell index)
-	{
-		return return_address_count * scrub_d_count +
-			index * scrub_r_count;
-	}
+  cell callsite_scrub_r(cell index) {
+    return return_address_count * scrub_d_count + index * scrub_r_count;
+  }
 
-	cell callsite_gc_roots(cell index)
-	{
-		return return_address_count * scrub_d_count
-			+ return_address_count * scrub_r_count
-			+ index * gc_root_count;
-	}
+  cell callsite_gc_roots(cell index) {
+    return return_address_count * scrub_d_count +
+           return_address_count * scrub_r_count + index * gc_root_count;
+  }
 
-	u32 lookup_base_pointer(cell index, cell derived_root)
-	{
-		return base_pointer_map()[index * derived_root_count + derived_root];
-	}
+  u32 lookup_base_pointer(cell index, cell derived_root) {
+    return base_pointer_map()[index * derived_root_count + derived_root];
+  }
 
-	cell return_address_index(cell return_address);
+  cell return_address_index(cell return_address);
 };
 
 }
