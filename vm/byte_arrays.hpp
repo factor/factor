cf585e928842daff8fceeb5f10b969ba7c318810
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 21:46:08 -0400

    VM: Refactor byte_arrays to Factor style

--- vm/byte_arrays.hpp
+++ vm/byte_arrays.hpp
@@ -1,25 +1,25 @@
-namespace factor
-{
+namespace factor {
 
 struct growable_byte_array {
-	cell count;
-	data_root<byte_array> elements;
+  cell count;
+  data_root<byte_array> elements;
 
-	explicit growable_byte_array(factor_vm *parent,cell capacity = 40) : count(0), elements(parent->allot_byte_array(capacity),parent) { }
+  explicit growable_byte_array(factor_vm* parent, cell capacity = 40)
+      : count(0), elements(parent->allot_byte_array(capacity), parent) {}
 
-	void grow_bytes(cell len);
-	void append_bytes(void *elts, cell len);
-	void append_byte_array(cell elts);
+  void grow_bytes(cell len);
+  void append_bytes(void* elts, cell len);
+  void append_byte_array(cell elts);
 
-	void trim();
+  void trim();
 };
 
 /* Allocates memory */
-template<typename Type> byte_array *factor_vm::byte_array_from_value(Type *value)
-{
-	byte_array *data = allot_uninitialized_array<byte_array>(sizeof(Type));
-	memcpy(data->data<char>(),value,sizeof(Type));
-	return data;
+template <typename Type>
+byte_array* factor_vm::byte_array_from_value(Type* value) {
+  byte_array* data = allot_uninitialized_array<byte_array>(sizeof(Type));
+  memcpy(data->data<char>(), value, sizeof(Type));
+  return data;
 }
 
 }
