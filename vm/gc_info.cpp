0903067f194e4b07f302a36e633a0c34e477cdd1
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:04:08 -0400

    VM: Refactor gc* to Factor style

--- vm/gc_info.cpp
+++ vm/gc_info.cpp
@@ -1,19 +1,16 @@
 #include "master.hpp"
 
-namespace factor
-{
+namespace factor {
 
-cell gc_info::return_address_index(cell return_address)
-{
-	u32 *return_address_array = return_addresses();
+cell gc_info::return_address_index(cell return_address) {
+  u32* return_address_array = return_addresses();
 
-	for(cell i = 0; i < return_address_count; i++)
-	{
-		if(return_address == return_address_array[i])
-			return i;
-	}
+  for (cell i = 0; i < return_address_count; i++) {
+    if (return_address == return_address_array[i])
+      return i;
+  }
 
-	return (cell)-1;
+  return (cell) - 1;
 }
 
 }
