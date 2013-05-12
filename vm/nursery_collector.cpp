6245a8c99d5997809dd98597c817379dd0447607
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:16:22 -0400

    VM: Refactor nursery_* to Factor style

--- vm/nursery_collector.cpp
+++ vm/nursery_collector.cpp
@@ -1,45 +1,42 @@
 #include "master.hpp"
 
-namespace factor
-{
+namespace factor {
 
-nursery_collector::nursery_collector(factor_vm *parent_) :
-	copying_collector<aging_space,nursery_policy>(
-		parent_,
-		parent_->data->aging,
-		nursery_policy(parent_)) {}
+nursery_collector::nursery_collector(factor_vm* parent_)
+    : copying_collector<aging_space, nursery_policy>(parent_,
+                                                     parent_->data->aging,
+                                                     nursery_policy(parent_)) {}
 
-void factor_vm::collect_nursery()
-{
-	/* Copy live objects from the nursery (as determined by the root set and
-	marked cards in aging and tenured) to aging space. */
-	nursery_collector collector(this);
+void factor_vm::collect_nursery() {
+  /* Copy live objects from the nursery (as determined by the root set and
+     marked cards in aging and tenured) to aging space. */
+  nursery_collector collector(this);
 
-	collector.trace_roots();
-	collector.trace_contexts();
+  collector.trace_roots();
+  collector.trace_contexts();
 
-	gc_event *event = current_gc->event;
+  gc_event* event = current_gc->event;
 
-	if(event) event->started_card_scan();
-	collector.trace_cards(data->tenured,
-		card_points_to_nursery,
-		simple_unmarker(card_points_to_nursery));
-	if(data->aging->here != data->aging->start)
-	{
-		collector.trace_cards(data->aging,
-			card_points_to_nursery,
-			full_unmarker());
-	}
-	if(event) event->ended_card_scan(collector.cards_scanned,collector.decks_scanned);
+  if (event)
+    event->started_card_scan();
+  collector.trace_cards(data->tenured, card_points_to_nursery,
+                        simple_unmarker(card_points_to_nursery));
+  if (data->aging->here != data->aging->start) {
+    collector.trace_cards(data->aging, card_points_to_nursery, full_unmarker());
+  }
+  if (event)
+    event->ended_card_scan(collector.cards_scanned, collector.decks_scanned);
 
-	if(event) event->started_code_scan();
-	collector.trace_code_heap_roots(&code->points_to_nursery);
-	if(event) event->ended_code_scan(collector.code_blocks_scanned);
+  if (event)
+    event->started_code_scan();
+  collector.trace_code_heap_roots(&code->points_to_nursery);
+  if (event)
+    event->ended_code_scan(collector.code_blocks_scanned);
 
-	collector.cheneys_algorithm();
+  collector.cheneys_algorithm();
 
-	data->reset_generation(&nursery);
-	code->points_to_nursery.clear();
+  data->reset_generation(&nursery);
+  code->points_to_nursery.clear();
 }
 
 }
