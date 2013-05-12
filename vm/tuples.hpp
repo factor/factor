5b1a9d753e565e97127b495957ae2ae9b3fc0c00
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:30:22 -0400

    VM: Refactor tuples.cpp/hpp to Factor style

--- vm/tuples.hpp
+++ vm/tuples.hpp
@@ -1,10 +1,8 @@
-namespace factor
-{
+namespace factor {
 
-inline static cell tuple_size(const tuple_layout *layout)
-{
-	cell size = untag_fixnum(layout->size);
-	return sizeof(tuple) + size * sizeof(cell);
+inline static cell tuple_size(const tuple_layout* layout) {
+  cell size = untag_fixnum(layout->size);
+  return sizeof(tuple) + size * sizeof(cell);
 }
 
 }
