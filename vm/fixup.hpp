95d34e8ee7f65234638c1938184f48e32b0cf98e
Erik Charlebois <erikcharlebois@gmail.com>
2013-05-11 22:00:47 -0400

    VM: Refactor fixup.hpp to Factor style

--- vm/fixup.hpp
+++ vm/fixup.hpp
@@ -1,46 +1,25 @@
-namespace factor
-{
+namespace factor {
 
-template<typename T>
-struct identity {
-	T operator()(T t)
-	{
-		return t;
-	}
+template <typename T> struct identity {
+  T operator()(T t) { return t; }
 };
 
 struct no_fixup {
-	static const bool translated_code_block_map = false;
+  static const bool translated_code_block_map = false;
 
-	object *fixup_data(object *obj)
-	{
-		return obj;
-	}
+  object* fixup_data(object* obj) { return obj; }
 
-	code_block *fixup_code(code_block *compiled)
-	{
-		return compiled;
-	}
+  code_block* fixup_code(code_block* compiled) { return compiled; }
 
-	object *translate_data(const object *obj)
-	{
-		return fixup_data((object *)obj);
-	}
+  object* translate_data(const object* obj) { return fixup_data((object*)obj); }
 
-	code_block *translate_code(const code_block *compiled)
-	{
-		return fixup_code((code_block *)compiled);
-	}
+  code_block* translate_code(const code_block* compiled) {
+    return fixup_code((code_block*)compiled);
+  }
 
-	cell size(object *obj)
-	{
-		return obj->size();
-	}
+  cell size(object* obj) { return obj->size(); }
 
-	cell size(code_block *compiled)
-	{
-		return compiled->size();
-	}
+  cell size(code_block* compiled) { return compiled->size(); }
 };
 
 }
