07cfd2c690337672e3eb4ba528895238bcd11103
Björn Lindqvist <bjourne@gmail.com>
2014-05-29 19:08:01 +0200

    VM: refactored factor_vm::unbox_array_size()

--- vm/math.cpp
+++ vm/math.cpp
@@ -191,24 +191,6 @@ void factor_vm::primitive_bignum_log2() {
 }
 
 /* Allocates memory */
-cell factor_vm::unbox_array_size_slow() {
-  if (tagged<object>(ctx->peek()).type() == BIGNUM_TYPE) {
-    bignum* zero = untag<bignum>(bignum_zero);
-    GC_BIGNUM(zero);
-    bignum* max = cell_to_bignum(array_size_max);
-    bignum* n = untag<bignum>(ctx->peek());
-    if (bignum_compare(n, zero) != bignum_comparison_less &&
-        bignum_compare(n, max) == bignum_comparison_less) {
-      ctx->pop();
-      return bignum_to_cell(n);
-    }
-  }
-
-  general_error(ERROR_ARRAY_SIZE, ctx->pop(), tag_fixnum(array_size_max));
-  return 0; /* can't happen */
-}
-
-/* Allocates memory */
 void factor_vm::primitive_fixnum_to_float() {
   ctx->replace(allot_float(fixnum_to_float(ctx->peek())));
 }
