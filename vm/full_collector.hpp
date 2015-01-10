b61eaa8a1662c403eaf45e5c176ab07a241ea9e8
Björn Lindqvist <bjourne@gmail.com>
2015-01-10 15:00:12 +0000

    VM: the full_collector class isn't needed since it is only used once

--- vm/full_collector.hpp
+++ vm/full_collector.hpp
@@ -22,11 +22,4 @@ struct full_policy {
   }
 };
 
-struct full_collector : collector<tenured_space, full_policy> {
-  code_block_visitor<gc_workhorse<tenured_space, full_policy> > code_visitor;
-
-  explicit full_collector(factor_vm* parent);
-  void trace_code_block(code_block* compiled);
-};
-
 }
