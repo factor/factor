0a6486ef7832f63f0e71d8cadbdac01f28e3992f
Björn Lindqvist <bjourne@gmail.com>
2015-05-04 03:57:35 +0200

    VM: the aging_policy and nursery_policy classes are so small that they can be in the same cpp file with the methods that use them

--- vm/nursery_collector.cpp
+++ vm/nursery_collector.cpp
@@ -2,6 +2,20 @@
 
 namespace factor {
 
+struct nursery_policy {
+  factor_vm* parent;
+
+  explicit nursery_policy(factor_vm* parent) : parent(parent) {}
+
+  bool should_copy_p(object* obj) {
+    return parent->data->nursery->contains_p(obj);
+  }
+
+  void promoted_object(object* obj) {}
+
+  void visited_object(object* obj) {}
+};
+
 void factor_vm::collect_nursery() {
   /* Copy live objects from the nursery (as determined by the root set and
      marked cards in aging and tenured) to aging space. */
