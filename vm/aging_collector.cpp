e6cc16fd1bcb1fd8ab1f7e8befd504532376ef36
Björn Lindqvist <bjourne@gmail.com>
2015-05-07 13:29:57 +0200

    VM: cleanups, like removing redundant paranthesis and removing method declarations that doesn't exist

--- vm/aging_collector.cpp
+++ vm/aging_collector.cpp
@@ -3,14 +3,11 @@
 namespace factor {
 
 struct aging_policy {
-  factor_vm* parent;
   aging_space* aging;
   tenured_space* tenured;
 
   explicit aging_policy(factor_vm* parent)
-      : parent(parent),
-        aging(parent->data->aging),
-        tenured(parent->data->tenured) {}
+      : aging(parent->data->aging), tenured(parent->data->tenured) {}
 
   bool should_copy_p(object* untagged) {
     return !(aging->contains_p(untagged) || tenured->contains_p(untagged));
