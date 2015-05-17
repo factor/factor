6ca83e7588c28800d31d8c0996f1debe9dc244ae
Björn Lindqvist <bjourne@gmail.com>
2015-05-17 10:11:48 +0200

    Reorganising bootstrap files
    All the bootstrap.factor scripts in cpu/ are run by the bootstrap to
    make boot images, but aren't used otherwise. So I think it's cleaner to
    put them in one directory inside the bootstrap hierarchy.

--- vm/os-macosx-x86.64.hpp
+++ vm/os-macosx-x86.64.hpp
@@ -68,7 +68,7 @@ inline static void uap_clear_fpu_status(void* uap) {
 }
 
 /* Must match the leaf-stack-frame-size, signal-handler-stack-frame-size,
-and stack-frame-size constants in basis/cpu/x86/64/unix/bootstrap.factor */
+and stack-frame-size constants in basis/bootstrap/assembler/x86.64.unix.factor */
 static const unsigned LEAF_FRAME_SIZE = 16;
 static const unsigned SIGNAL_HANDLER_STACK_FRAME_SIZE = 160;
 static const unsigned JIT_FRAME_SIZE = 32;
