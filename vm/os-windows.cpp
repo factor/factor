81d27ecbec49a3f6023c740db808d7e0441c2d2f
Björn Lindqvist <bjourne@gmail.com>
2015-08-26 11:58:39 +0200

    VM: signal_fault_pc probably should be set here...

--- vm/os-windows.cpp
+++ vm/os-windows.cpp
@@ -195,6 +195,7 @@ LONG factor_vm::exception_handler(PEXCEPTION_RECORD e, void* frame, PCONTEXT c,
   switch (e->ExceptionCode) {
     case EXCEPTION_ACCESS_VIOLATION:
       signal_fault_addr = e->ExceptionInformation[1];
+      signal_fault_pc = c->EIP;
       verify_memory_protection_error(signal_fault_addr);
       dispatch_signal_handler((cell*)&c->ESP, (cell*)&c->EIP,
                               (cell)factor::memory_signal_handler_impl);
