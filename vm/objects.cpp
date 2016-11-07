2308ca06e0a388353e2e872a2b6b6454b8bdf2d4
Björn Lindqvist <bjourne@gmail.com>
2016-11-07 23:43:50 +0100

    Revert "VM: undoing 22bb0cfecfc5c09953b613e3ad01d2a713585ea7 (#1513)"
    This reverts commit e9c72baf2ec08cf510a53c25ce65602fcda82222.

--- vm/objects.cpp
+++ vm/objects.cpp
@@ -93,11 +93,10 @@ void factor_vm::primitive_become() {
   std::map<object*, object*> become_map;
 
   for (cell i = 0; i < capacity; i++) {
-    tagged<object> old_obj(array_nth(old_objects, i));
-    tagged<object> new_obj(array_nth(new_objects, i));
-
-    if (old_obj != new_obj)
-      become_map[old_obj.untagged()] = new_obj.untagged();
+    cell old_ptr = array_nth(old_objects, i);
+    cell new_ptr = array_nth(new_objects, i);
+    if (old_ptr != new_ptr)
+      become_map[untag<object>(old_ptr)] = untag<object>(new_ptr);
   }
 
   // Update all references to old objects to point to new objects
