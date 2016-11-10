factor/factor

commit f20ee7a53b264fda515d2a55c8fbea26261de016

Use new threaded-server features in concurrency.distributed

basis/concurrency/distributed/distributed-docs.factor
@@ -1,22 +1,9 @@
 USING: help.markup help.syntax concurrency.messaging threads ;
 IN: concurrency.distributed
 
-HELP: local-node
-{ $var-description "A variable containing the node the current thread is running on." } ;
-
-HELP: start-node
-{ $values { "port" "a port number between 0 and 65535" } { "threaded-server" "a threaded-server tuple" } }
-{ $description "Starts a node server for receiving messages from remote Factor instances." } ;
-
 ARTICLE: "concurrency.distributed.example" "Distributed Concurrency Example"
-"For a Factor instance to be able to send and receive distributed "
-"concurrency messages it must first have " { $link start-node } " called."
-$nl
-"In one factor instance call " { $link start-node } " with the port 9000, "
-"and in another with the port 9001."
-$nl
 "In this example the Factor instance associated with port 9000 will run "
-"a thread that sits receiving messages and printing the received message "
+"a thread that receives and prints messages "
 "in the listener. The code to start the thread is: "
 { $examples
     { $unchecked-example
@@ -51,11 +38,9 @@ $nl
 
 ARTICLE: "concurrency.distributed" "Distributed message passing"
 "The " { $vocab-link "concurrency.distributed" } " implements transparent distributed message passing, inspired by Erlang and Termite."
-{ $subsections start-node }
 "Instances of " { $link thread } " can be sent to remote threads, at which point they are converted to objects holding the thread ID and the current node's host name:"
 { $subsections remote-thread }
 "The " { $vocab-link "serialize" } " vocabulary is used to convert Factor objects to byte arrays for transfer over a socket." 
 { $subsections "concurrency.distributed.example" } ;
-
 
 ABOUT: "concurrency.distributed"

basis/concurrency/distributed/distributed.factor
@@ -22,8 +22,6 @@ PRIVATE>
 : get-remote-thread ( name -- thread )
     dup registered-remote-threads at [ ] [ threads at ] ?if ;
 
-SYMBOL: local-node
-
 : handle-node-client ( -- )
     deserialize
     [ first2 get-remote-thread send ] [ stop-this-server ] if* ;
@@ -33,12 +31,6 @@ SYMBOL: local-node
         swap >>insecure
         "concurrency.distributed" >>name
         [ handle-node-client ] >>handler ;
-
-: (start-node) ( addrspec addrspec -- threaded-server )
-    local-node set-global <node-server> start-server ;
-
-: start-node ( port -- threaded-server )
-    host-name over <inet> (start-node) ;
 
 TUPLE: remote-thread node id ;
 
@@ -52,8 +44,7 @@ M: remote-thread send ( message thread -- )
     send-remote-message ;
 
 M: thread (serialize) ( obj -- )
-    id>> [ local-node get-global ] dip <remote-thread>
-    (serialize) ;
+    id>> [ insecure-addr ] dip <remote-thread> (serialize) ;
 
 : stop-node ( node -- )
     f swap send-remote-message ;

basis/concurrency/distributed/distributed-tests.factor
@@ -1,33 +1,39 @@
 USING: tools.test concurrency.distributed kernel io.files
-io.files.temp io.directories arrays io.sockets system
+io.files.temp io.directories arrays io.sockets system calendar
 combinators threads math sequences concurrency.messaging
-continuations accessors prettyprint ;
+continuations accessors prettyprint io.servers.connection ;
 FROM: concurrency.messaging => receive send ;
 IN: concurrency.distributed.tests
 
-: test-node ( -- addrspec )
+CONSTANT: test-ip "127.0.0.1"
+
+: test-node-server ( -- threaded-server )
     {
         { [ os unix? ] [ "distributed-concurrency-test" temp-file <local> ] }
-        { [ os windows? ] [ "127.0.0.1" 1238 <inet4> ] }
+        { [ os windows? ] [ test-ip 0 <inet4> ] }
+    } cond <node-server> ;
+
+: test-node-client ( -- addrspec )
+    {
+        { [ os unix? ] [ "distributed-concurrency-test" temp-file <local> ] }
+        { [ os windows? ] [ test-ip insecure-port <inet4> ] }
     } cond ;
 
+    
 [ ] [ [ "distributed-concurrency-test" temp-file delete-file ] ignore-errors ] unit-test
 
-[ ] [ test-node dup (start-node) drop ] unit-test
+test-node-server [
+    [ ] [
+        [
+            receive first2 [ 3 + ] dip send
+            "thread-a" unregister-remote-thread
+        ] "Thread A" spawn
+        "thread-a" register-remote-thread
+    ] unit-test
 
-[ ] [
-    [
-        receive first2 [ 3 + ] dip send
-        "thread-a" unregister-remote-thread
-    ] "Thread A" spawn
-    "thread-a" register-remote-thread
-] unit-test
-
-[ 8 ] [
-    5 self 2array
-    test-node "thread-a" <remote-thread> send
-
-    receive
-] unit-test
-
-[ ] [ test-node stop-node ] unit-test
+    [ 8 ] [
+        5 self 2array
+        test-node-client "thread-a" <remote-thread> send
+        100 seconds receive-timeout
+    ] unit-test
+] with-threaded-server

commit 299fe8b416776370f5c7b2abd752c035cef8926c

Fix serializing of remote threads in concurrency.distributed
The commit f20ee7a53b264fda515d2a55c8fbea26261de016 broke
serialization of remote threads and removed the local node
aspect of distributed messaging. This corrects that.

basis/concurrency/distributed/distributed-docs.factor
@@ -1,5 +1,12 @@
-USING: help.markup help.syntax concurrency.messaging threads ;
+USING: help.markup help.syntax concurrency.messaging io.servers threads ;
 IN: concurrency.distributed
+
+HELP: local-node
+{ $var-description "A variable containing the " { $link threaded-server } " the current node is running on." } ;
+
+HELP: start-node
+{ $values { "addrspec" "an addrspec to listen on" } }
+{ $description "Starts a " { $link threaded-server } " for receiving messages from remote Factor instances." } ;
 
 ARTICLE: "concurrency.distributed.example" "Distributed Concurrency Example"
 "In this example the Factor instance associated with port 9000 will run "
@@ -11,7 +18,7 @@ ARTICLE: "concurrency.distributed.example" "Distributed Concurrency Example"
         "[ log-message ] \"logger\" spawn dup name>> register-remote-thread"
     }
 }
-"This spawns a thread waits for the messages. It registers that thread as a "
+"This spawns a thread waits for the messages. It registers that thread as "
 "able to be accessed remotely using " { $link register-remote-thread } "."
 $nl
 "The second Factor instance, the one associated with port 9001, can send "
@@ -38,7 +45,8 @@ $nl
 
 ARTICLE: "concurrency.distributed" "Distributed message passing"
 "The " { $vocab-link "concurrency.distributed" } " implements transparent distributed message passing, inspired by Erlang and Termite." $nl
-"Instances of " { $link thread } " can be sent to remote threads, at which point they are converted to objects holding the thread ID and the current node's host name:"
+{ $subsections local-node start-node }
+"Instances of " { $link thread } " can be sent to remote nodes, at which point they are converted to objects holding the thread ID and the current node's addrspec:"
 { $subsections remote-thread }
 "The " { $vocab-link "serialize" } " vocabulary is used to convert Factor objects to byte arrays for transfer over a socket."
 { $subsections "concurrency.distributed.example" } ;

basis/concurrency/distributed/distributed.factor
@@ -22,6 +22,8 @@ PRIVATE>
 : get-remote-thread ( name -- thread )
     dup registered-remote-threads at [ ] [ threads at ] ?if ;
 
+SYMBOL: local-node
+
 : handle-node-client ( -- )
     deserialize
     [ first2 get-remote-thread send ] [ stop-this-server ] if* ;
@@ -31,6 +33,9 @@ PRIVATE>
         swap >>insecure
         "concurrency.distributed" >>name
         [ handle-node-client ] >>handler ;
+
+: start-node ( addrspec -- )
+    <node-server> start-server local-node set-global ;
 
 TUPLE: remote-thread node id ;
 
@@ -44,10 +49,10 @@ M: remote-thread send ( message thread -- )
     send-remote-message ;
 
 M: thread (serialize) ( obj -- )
-    id>> [ insecure-addr ] dip <remote-thread> (serialize) ;
+    id>> [ local-node get insecure>> ] dip <remote-thread> (serialize) ;
 
-: stop-node ( node -- )
-    f swap send-remote-message ;
+: stop-node ( -- )
+    local-node get insecure>> f swap send-remote-message ;
 
 [
     H{ } clone \ registered-remote-threads set-global