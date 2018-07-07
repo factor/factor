factor/factor

commit 8c158aa68ff5a478349392f89fee6c50e0bd16af

help.lint.checks: ignore words without defined word-help.
This allows us to skip words that use the $default help.

basis/help/lint/checks/checks.factor
@@ -78,6 +78,7 @@ SYMBOL: vocab-articles
         [ parsing-word? ]
         [ "declared-effect" word-prop not ]
         [ constant? ]
+        [ "word-help" word-prop not ]
     } 1|| ;
 
 : skip-check-values? ( word element -- ? )

commit 06b07b9e9c66734579e9a05854d852a7a5137c96

help.lint.checks: Fix regression in help-lint introduced in 8c158aa68ff5a478349392f89fee6c50e0bd16af.
We haven't been checking words in help-lint for two years! Ack!

basis/help/lint/checks/checks.factor
@@ -99,7 +99,7 @@ SYMBOL: vocab-articles
         [ parsing-word? ]
         [ "declared-effect" word-prop not ]
         [ constant? ]
-        [ "word-help" word-prop not ]
+        [ "help" word-prop not ]
     } 1|| ;
 
 : skip-check-values? ( word element -- ? )