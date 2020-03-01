927680ebfd076aa500dc4b404f67804856b3778b
John Benediktsson <mrjbq7@gmail.com>
2020-03-01 12:20:32 -0800

    webapps.wiki: simplify and fix resize issue on mobile.

--- extra/webapps/wiki/wiki-contents.js
+++ extra/webapps/wiki/wiki-contents.js
@@ -1,27 +1,11 @@
-
-function resetSidebar() {
-    var l = document.getElementById("left");
-    var m = document.getElementById("menu");
-    l.style.position = "";
-    l.style.display = "";
-    l.style.marginTop = "";
-    m.style.backgroundColor = "";
-}
-
-window.addEventListener("resize", resetSidebar);
-
 function toggleSidebar() {
     var l = document.getElementById("left");
     var m = document.getElementById("menu");
     if (l.style.display === "") {
-        l.style.position = "absolute";
         l.style.display = "inline";
-        l.style.marginTop = "40px";
         m.style.backgroundColor = "#f3f2ea";
     } else {
-        l.style.position = "";
         l.style.display = "";
-        l.style.marginTop = "";
         m.style.backgroundColor = "";
     }
 }
