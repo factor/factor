15a21ce2c1801f3ceb5e0bd771bc6a5aa5b669e0
Doug Coleman <doug.coleman@gmail.com>
2020-11-17 16:43:37 -0600

    build.sh: Add arm64 for new Mac Mini

--- build.sh
+++ build.sh
@@ -303,6 +303,7 @@ find_architecture() {
        *86) ARCH=x86;;
        *86_64) ARCH=x86;;
        aarch64) ARCH=arm64;;
+       arm64) ARCH=arm64;;
        iPhone5*[3-9]) ARCH=arm64;;
        iPhone[6-9]*) ARCH=arm64;;
        iPhone[1-9][0-9]*) ARCH=arm64;;
