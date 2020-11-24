13b2a2fab2968fcc6ce783d88375f7422dc381b2
Doug Coleman <doug.coleman@gmail.com>
2020-11-23 19:01:26 -0600

    vm: Add a bunch of half-baked arm files. Also fix some .h issues.

---
+++ vm/cpu-arm.32.hpp
@@ -0,0 +1,8 @@
+namespace factor {
+
+#define FACTOR_CPU_STRING "arm.32"
+
+register cell ds asm("r5");
+register cell rs asm("r6");
+
+}
---
+++ vm/cpu-arm.64.hpp
@@ -0,0 +1,13 @@
+namespace factor {
+
+#define FACTOR_CPU_STRING "arm.64"
+
+// register cell ds asm("r5");
+// register cell rs asm("r6");
+inline static void flush_icache(cell start, cell len) {
+  // int result;
+}
+
+
+
+}
