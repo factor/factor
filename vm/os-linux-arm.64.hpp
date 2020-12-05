0dcc296b7f276b0c8b6d40201dbcd7a0db6356b7
Doug Coleman <doug.coleman@gmail.com>
2020-12-04 19:09:15 -0600

    vm: Move arm to arm64.

--- vm/os-linux-arm.64.hpp
+++ vm/os-linux-arm.64.hpp
@@ -73,37 +73,6 @@ static const unsigned JIT_FRAME_SIZE = 32;
 static const unsigned char call_opcode = 0xe8;
 static const unsigned char jmp_opcode = 0xe9;
 
-inline static unsigned char call_site_opcode(cell return_address) {
-  return *(unsigned char*)(return_address - 5);
-}
-
-inline static void check_call_site(cell return_address) {
-  unsigned char opcode = call_site_opcode(return_address);
-  FACTOR_ASSERT(opcode == call_opcode || opcode == jmp_opcode);
-  (void)opcode; // suppress warning when compiling without assertions
-}
-
-inline static void* get_call_target(cell return_address) {
-  check_call_site(return_address);
-  return (void*)(*(int*)(return_address - 4) + return_address);
-}
-
-inline static void set_call_target(cell return_address, cell target) {
-  check_call_site(return_address);
-  *(int*)(return_address - 4) = (uint32_t)(target - return_address);
-}
-
-inline static bool tail_call_site_p(cell return_address) {
-  switch (call_site_opcode(return_address)) {
-    case jmp_opcode:
-      return true;
-    case call_opcode:
-      return false;
-    default:
-      abort();
-      return false;
-  }
-}
 
 static const unsigned SIGNAL_HANDLER_STACK_FRAME_SIZE = 192;
 
