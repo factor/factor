b1ed846276d12e9dc31719f543a7f125f3daa14a
Doug Coleman <doug.coleman@gmail.com>
2021-03-17 00:22:55 -0500

    icache: something like this might flush the icache

--- vm/cpu-arm.64.hpp
+++ vm/cpu-arm.64.hpp
@@ -5,9 +5,11 @@ namespace factor {
 // register cell ds asm("r5");
 // register cell rs asm("r6");
 inline static void flush_icache(cell start, cell len) {
-  // int result;
+  //__asm("ic ialluis");
+  //__asm volatile ("dmb sy" ::: "memory");
+
+  //dsb(nsh);
+  //isb();
 }
-
-
 
 }
