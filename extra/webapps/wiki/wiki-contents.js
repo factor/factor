df4cc29e96cde183633c4c6bc5e2db9736e6eb1f
John Benediktsson <mrjbq7@gmail.com>
2022-05-17 10:50:06 -0700

    webapps.wiki: dark mode

--- extra/webapps/wiki/wiki-contents.js
+++ extra/webapps/wiki/wiki-contents.js
@@ -3,7 +3,10 @@ function toggleSidebar() {
     var m = document.getElementById("menu");
     if (l.style.display === "") {
         l.style.display = "inline";
-        m.style.backgroundColor = "#f3f2ea";
+        var darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (!darkMode) {
+            m.style.backgroundColor = "#f3f2ea";
+        }
     } else {
         l.style.display = "";
         m.style.backgroundColor = "";
