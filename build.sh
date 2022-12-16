dbe0ccedd6cb5d0ac9a73dcb6849af3e8a246fb6
Doug Coleman <doug.coleman@gmail.com>
2022-12-16 16:02:08 -0600

    build.sh: Fix hang because of wget/ipv6 on some of my machines.
    My linux box prefers an ipv6 address for some reason which doesn't resolve.
    
    Might need to update factorcode ipv6 settings, too.

--- build.sh
+++ build.sh
@@ -595,7 +595,7 @@ current_git_branch() {
 
 check_url() {
     if [[ $DOWNLOADER_NAME == 'wget' ]]; then
-        if [[ $(wget -S --spider $1 2>&1 | grep 'HTTP/1.1 200 OK') ]]; then
+        if [[ $(wget -S --spider --prefer-family=IPv4 $1 2>&1 | grep 'HTTP/1.1 200 OK') ]]; then
             return 0
         else
             return 1
@@ -614,7 +614,9 @@ check_url() {
 set_boot_image_vars() {
     set_current_branch
     local url="https://downloads.factorcode.org/images/${CURRENT_BRANCH}/checksums.txt"
+    $ECHO "Getting checksum from ${url}"
     check_url $url
+    $ECHO "got checksum!"
     if [[ $? -eq 0 ]]; then
         CHECKSUM_URL="$url"
         BOOT_IMAGE_URL="https://downloads.factorcode.org/images/${CURRENT_BRANCH}/${BOOT_IMAGE}"
