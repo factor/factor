132c2a47d3e94d93b38f9cd4fd5d1ba17ea93d0e
Doug Coleman <doug.coleman@gmail.com>
2023-01-24 17:02:18 -0600

    Revert "vm/slot_visitor: handle F_TYPE, the only unhandled type"
    This reverts commit f62df26e9463f718a2ed61fd704f167867871e80.

--- vm/slot_visitor.hpp
+++ vm/slot_visitor.hpp
@@ -69,7 +69,6 @@ inline cell object::slot_count(Fixup fixup) const {
   } else {
     switch (t) {
       // these objects do not refer to other objects at all
-      case F_TYPE:
       case FLOAT_TYPE:
       case BYTE_ARRAY_TYPE:
       case BIGNUM_TYPE:
