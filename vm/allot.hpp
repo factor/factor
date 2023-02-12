b2ca0c0edbab2e20f6b789d90270f6feb312de53
Razetime <rraghu.11502@gmail.com>
2023-02-12 21:09:05 +0530

    basic shadowed name changes

--- vm/allot.hpp
+++ vm/allot.hpp
@@ -64,18 +64,18 @@ inline object* factor_vm::allot_large_object(cell type, cell size) {
 inline object* factor_vm::allot_object(cell type, cell size) {
   FACTOR_ASSERT(!current_gc);
 
-  bump_allocator *nursery = data->nursery;
+  bump_allocator *dnursery = data->nursery;
 
   // If the object is bigger than the nursery, allocate it in tenured space
-  if (size >= nursery->size)
+  if (size >= dnursery->size)
     return allot_large_object(type, size);
 
   // If the object is smaller than the nursery, allocate it in the nursery,
   // after a GC if needed
-  if (nursery->here + size > nursery->end)
+  if (dnursery->here + size > dnursery->end)
     primitive_minor_gc();
 
-  object* obj = nursery->allot(size);
+  object* obj = dnursery->allot(size);
   obj->initialize(type);
 
   return obj;
