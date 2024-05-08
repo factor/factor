d70293b65ae6988ba5f74243d20225b07b016c67
nomennescio <nomennescio@factorcode.org>
2024-05-08 11:42:54 +0100

    Ignore clang++ specific warning

--- vm/zstd.cpp
+++ vm/zstd.cpp
@@ -1,6 +1,9 @@
 // Copyright (C) 2022-2024 nomennescio
 // See https://factorcode.org/license.txt for BSD license.
 
+// clang++ issue: https://github.com/nlohmann/json/pull/1551
+#pragma clang diagnostic ignored "-Wc++1z-extensions"
+
 #include "master.hpp"
 
 namespace lib { namespace zstd { extern "C" {
