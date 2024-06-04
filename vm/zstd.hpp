bb2de88252e7d13817b79796956d5344353476df
nomennescio <nomennescio@factorcode.org>
2024-06-04 16:27:07 +0200

    zstd: limit visibility of unused symbols
    experimental

--- vm/zstd.hpp
+++ vm/zstd.hpp
@@ -4,9 +4,24 @@
 // zstd.h is copied from github.com/facebook/zstd.git using
 // $ cp zstd/lib/zstd.h factor/vm/zstd.h
 
-namespace lib { namespace zstd { extern "C" {
+// because we import external code here, which we don't want to alter, we need to handle integration here
+
+// ignore unused function warnings
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wunused-function"
+#elif defined (__GNUC__)
+#pragma GCC diagnostic ignored "-Wunused-function"
+#endif
+
+namespace lib { namespace zstd {
+  namespace c { extern "C" {
+#define ZSTDLIB_VISIBILITY static
 #define ZSTD_STATIC_LINKING_ONLY // enable advanced experimental functions
 // update the following line if zstd.h is updated
 // origin : git SHA1: 794ea1b0 tag: v1.5.6
 #include "zstd.h"
-} } }
+  } }
+  extern size_t ZSTD_decompress (void* dst, size_t dstCapacity, const void* src, size_t compressedSize);
+  extern unsigned ZSTD_isError (size_t code);
+  extern const char* ZSTD_getErrorName (size_t code);
+} }
