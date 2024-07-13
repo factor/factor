6071c8cd4c81643a7ba6e66d0a8b66220746c095
J. Ryan Stinnett <jryans@gmail.com>
2024-07-13 21:14:36 +0100

    zstd: Remove zstd source and static integration

--- vm/zstd.cpp
+++ vm/zstd.cpp
@@ -1,30 +1,9 @@
 // Copyright (C) 2022-2024 nomennescio
 // See https://factorcode.org/license.txt for BSD license.
 
-#if defined(__clang__)
-// clang++ issue: https://github.com/nlohmann/json/pull/1551
-#pragma clang diagnostic ignored "-Wc++1z-extensions"
-#endif
-
 #include "master.hpp"
 
-// zstd.c is generated from github.com/facebook/zstd.git using
-// $ cd zstd/build/single_file_libs
-// $ python combine.py -r ../../lib -o zstd.c zstd-in.c
-// $ cp zstd.c ../../../factor/vm/zstd.c
-
 namespace lib { namespace zstd {
-  namespace c { extern "C" {
-// update the following line if zstd.c is updated
-// origin : git SHA1: 794ea1b0 tag: v1.5.6
-BEGIN_PRAGMA (diagnostic ignored "-Wunused-function") // ignore unused function warnings
-#include "zstd.c"
-END_PRAGMA
-  } }
-  size_t ZSTD_decompress (void* dst, size_t dstCapacity, const void* src, size_t compressedSize) { return c::ZSTD_decompress (dst, dstCapacity, src, compressedSize); }
-  unsigned ZSTD_isError (size_t code) { return c::ZSTD_isError (code); }
-  const char* ZSTD_getErrorName (size_t code) { return c::ZSTD_getErrorName (code); }
-
   void zstd_lib::open() {
     handle = factor::native_dlopen(ZSTD_LIB);
     if (!handle)
