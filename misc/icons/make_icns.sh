3040fd8e8a883b2b5a08f1a09cc881b80697647d
John Benediktsson <mrjbq7@gmail.com>
2025-08-26 10:46:53 -0700

    misc/icons: adding make scripts

---
+++ misc/icons/make_icns.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ICONS=(
+  "icon_16x16.png:16x16"
+  "icon_16x16@2x.png:32x32"
+  "icon_32x32.png:32x32"
+  "icon_32x32@2x.png:64x64"
+  "icon_128x128.png:128x128"
+  "icon_128x128@2x.png:256x256"
+  "icon_256x256.png:256x256"
+  "icon_256x256@2x.png:512x512"
+  "icon_512x512.png:512x512"
+  "icon_512x512@2x.png:1024x1024"
+)
+
+IMAGE_DIR="$(pwd)"
+
+# Check if needed files exist
+for ICON in "${ICONS[@]}"; do
+  IFS=":" read -r NAME SIZE <<< "$ICON"
+  if [[ ! -f "$IMAGE_DIR/$NAME" ]]; then
+    echo "error: $NAME missing in $IMAGE_DIR"
+    exit 1
+  fi
+done
+
+ICONSET_DIR="icon.iconset"
+mkdir -p "$ICONSET_DIR"
+
+for ICON in "${ICONS[@]}"; do
+  IFS=":" read -r NAME SIZE <<< "$ICON"
+  cp "$IMAGE_DIR/$NAME" "$ICONSET_DIR/"
+done
+
+iconutil -c icns "$ICONSET_DIR"
+
+if [[ -f "icon.icns" ]]; then
+  echo "The .icns was succesfully created."
+else
+  echo "There was a problem while creating the .icns file."
+  exit 1
+fi
---
+++ misc/icons/make_ico.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+magick icon_256x256.png icon_128x128.png icon_64x64.png icon_32x32.png icon_16x16.png icon.ico
