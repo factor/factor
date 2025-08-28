e701de1856b2a0214f29699843eb9f6a0120257d
John Benediktsson <mrjbq7@gmail.com>
2025-08-28 08:48:40 -0700

    misc/icons: add @3x

--- misc/icons/make_icns.sh
+++ misc/icons/make_icns.sh
@@ -3,14 +3,19 @@
 ICONS=(
   "icon_16x16.png:16x16"
   "icon_16x16@2x.png:32x32"
+  "icon_16x16@3x.png:48x48"
   "icon_32x32.png:32x32"
   "icon_32x32@2x.png:64x64"
+  "icon_32x32@3x.png:96x96"
   "icon_128x128.png:128x128"
   "icon_128x128@2x.png:256x256"
+  "icon_128x128@3x.png:384x384"
   "icon_256x256.png:256x256"
   "icon_256x256@2x.png:512x512"
+  "icon_256x256@3x.png:768x768"
   "icon_512x512.png:512x512"
   "icon_512x512@2x.png:1024x1024"
+  "icon_512x512@3x.png:1536x1536"
 )
 
 IMAGE_DIR="$(pwd)"
