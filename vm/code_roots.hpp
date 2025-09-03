bd6ddf96e3dc9eb8bc63215df4551e4e79b93f4f
John Benediktsson <mrjbq7@gmail.com>
2025-09-03 13:39:14 -0700

    Revert "vm: Fix undefined behavior in code_roots during GC and compaction"
    This reverts commit fb89f839bf5e4065aa04df738bb26da1e92bcf02.

--- vm/code_roots.hpp
+++ vm/code_roots.hpp
@@ -7,17 +7,22 @@ struct code_root {
 
   void push() { parent->code_roots.push_back(this); }
 
-  code_root(cell value, factor_vm* parent)
-      : value(value), valid(true), parent(parent) {
+  code_root(cell val, factor_vm* vm)
+      : value(val), valid(true), parent(vm) {
     push();
   }
 
   ~code_root() {
-    if (parent) {
-      FACTOR_ASSERT(parent->code_roots.back() == this);
-      parent->code_roots.pop_back();
-    }
+    parent->code_roots.remove(this);
   }
+
+  // Disable copy operations to prevent double-free
+  code_root(const code_root&) = delete;
+  code_root& operator=(const code_root&) = delete;
+  
+  // Move operations could be implemented if needed
+  code_root(code_root&&) = delete;
+  code_root& operator=(code_root&&) = delete;
 };
 
 }
